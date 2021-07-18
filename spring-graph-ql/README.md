4b575ea6744862e1510481a629233e2ea7feb561
rajadileepkolli <rajadileepkolli@gmail.com>
2021-07-18 05:45:26 +0530

    Initial setup

---
+++ spring-graph-ql/README.md
@@ -0,0 +1,12 @@
+# spring-graph-ql
+
+
+### Run tests
+`$ ./gradlew clean build`
+
+### Run locally
+`$ ./gradlew bootRun -Plocal`
+
+### Useful Links
+* Swagger UI: http://localhost:8080/swagger-ui/index.html
+* Actuator Endpoint: http://localhost:8080/actuator
---
+++ spring-graph-ql/src/main/java/com/example/graphql/config/ApplicationProperties.java
@@ -0,0 +1,11 @@
+package com.example.graphql.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties("application")
+public class ApplicationProperties {
+    private String endpointUri;
+    private String region;
+}
