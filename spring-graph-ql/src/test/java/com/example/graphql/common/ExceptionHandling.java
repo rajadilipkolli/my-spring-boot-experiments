28c037b4b98341cb0d965b858326c6b7e2c1b597
rajadileepkolli <rajadileepkolli@gmail.com>
2021-10-06 18:39:46 +0530

    attempt to fix junits issue

--- spring-graph-ql/src/test/java/com/example/graphql/common/ExceptionHandling.java
+++ spring-graph-ql/src/test/java/com/example/graphql/common/ExceptionHandling.java
@@ -1,10 +1,14 @@
-package com.example.graphql.config;
+package com.example.graphql.common;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.zalando.problem.spring.webflux.advice.ProblemHandling;
 import org.zalando.problem.spring.webflux.advice.validation.OpenApiValidationAdviceTrait;
 
+import static com.example.graphql.utils.AppConstants.PROFILE_TEST;
+
+@Profile(PROFILE_TEST)
 @ControllerAdvice
 public final class ExceptionHandling implements ProblemHandling, OpenApiValidationAdviceTrait {
-    
+
 }
