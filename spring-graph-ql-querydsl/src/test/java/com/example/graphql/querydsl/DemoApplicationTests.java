004e23ee6315e66e2769cba65aa583fcd0e852dd
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2021-10-11 13:02:50 +0530

    adds basic structure

---
+++ spring-graph-ql-querydsl/src/test/java/com/example/graphql/querydsl/DemoApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.graphql.querydsl;
+
+import com.example.graphql.querydsl.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class DemoApplicationTests extends AbstractIntegrationTest {
+
+  @Test
+  void contextLoads() {}
+}
