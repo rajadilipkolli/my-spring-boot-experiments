e230ed4c364131d264ebd6533341a8fdff99cd8a
rajadileepkolli <rajadileepkolli@gmail.com>
2021-10-14 01:48:52 +0530

    initial commit of startegy-plugin

---
+++ strategy-plugin/src/main/java/com/example/plugin/strategyplugin/StrategyPluginApplication.java
@@ -0,0 +1,13 @@
+package com.example.plugin.strategyplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyPluginApplication.class, args);
+	}
+
+}
---
+++ strategy-plugin/src/test/java/com/example/plugin/strategyplugin/StrategyPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plugin.strategyplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
