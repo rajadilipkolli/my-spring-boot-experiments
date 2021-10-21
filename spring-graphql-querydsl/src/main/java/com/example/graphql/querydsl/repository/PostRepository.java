fc5b8140e8e4e9580d7ce22628e80606d111df7c
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2021-10-21 20:43:33 +0530

    adds datasource proxy to print queries

--- spring-graphql-querydsl/src/main/java/com/example/graphql/querydsl/repository/PostRepository.java
+++ spring-graphql-querydsl/src/main/java/com/example/graphql/querydsl/repository/PostRepository.java
@@ -1,9 +1,10 @@
 package com.example.graphql.querydsl.repository;
 
 import com.example.graphql.querydsl.entities.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.graphql.data.GraphQlRepository;
 
 @GraphQlRepository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository CrudRepository<Post, String>, QuerydslPredicateExecutor<Post> {
 }
