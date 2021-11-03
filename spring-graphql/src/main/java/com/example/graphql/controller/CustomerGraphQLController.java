40f6d7ce06dccf3bc409576aaf103af6ad25c042
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2021-11-03 14:00:32 +0530

    adds BatchMapping for fetching data

--- spring-graphql/src/main/java/com/example/graphql/controller/CustomerGraphQLController.java
+++ spring-graphql/src/main/java/com/example/graphql/controller/CustomerGraphQLController.java
@@ -6,20 +6,24 @@ import com.example.graphql.repository.CustomerRepository;
 import com.example.graphql.repository.OrdersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@Validated
 @RequiredArgsConstructor
-@Valid
 public class CustomerGraphQLController {
 
   private final CustomerRepository customerRepository;
@@ -36,9 +40,34 @@ public class CustomerGraphQLController {
     return this.customerRepository.findByNameIgnoringCase(name);
   }
 
-  @SchemaMapping(typeName = "Customer")
-  Flux<Orders> orders(Customer customer) {
-    return this.ordersRepository.findByCustomerId(customer.id());
+  //  @SchemaMapping(typeName = "Customer")
+  //  Flux<Orders> orders(Customer customer) {
+  //    return this.ordersRepository.findByCustomerId(customer.id());
+  //  }
+
+  // replacement for above code
+  @BatchMapping(typeName = "Customer")
+  public Mono<Map<Customer, List<Orders>>> orders(List<Customer> customers) {
+    var keys = customers.stream().map(Customer::id).toList();
+    return this.ordersRepository
+        .findByCustomerIdIn(keys)
+        .collectMultimap(Orders::customerId)
+        .map(
+            customerOrderMap -> {
+              var result = new HashMap<Customer, List<Orders>>();
+              customerOrderMap
+                  .keySet()
+                  .forEach(
+                      customerId -> {
+                        var customer =
+                            customers.stream()
+                                .filter(cust -> cust.id().equals(customerId))
+                                .toList()
+                                .get(0);
+                        result.put(customer, new ArrayList<>(customerOrderMap.get(customerId)));
+                      });
+              return result;
+            });
   }
 
   @MutationMapping
