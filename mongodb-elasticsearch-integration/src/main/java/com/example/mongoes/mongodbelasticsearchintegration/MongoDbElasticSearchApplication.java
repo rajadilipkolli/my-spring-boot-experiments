b7564bc86ddd321eb4b9859595675a12562e3473
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2021-11-12 18:55:09 +0530

    renamesfile

--- mongodb-elasticsearch-integration/src/main/java/com/example/mongoes/mongodbelasticsearchintegration/MongoDbElasticSearchApplication.java
+++ mongodb-elasticsearch-integration/src/main/java/com/example/mongoes/mongodbelasticsearchintegration/MongoDbElasticSearchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleApplicationWhereDataIsSavedInMongoDbAndSearchedInElasticSearchApplication {
+public class MongoDbElasticSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplicationWhereDataIsSavedInMongoDbAndSearchedInElasticSearchApplication.class, args);
+		SpringApplication.run(MongoDbElasticSearchApplication.class, args);
 	}
 
 }
