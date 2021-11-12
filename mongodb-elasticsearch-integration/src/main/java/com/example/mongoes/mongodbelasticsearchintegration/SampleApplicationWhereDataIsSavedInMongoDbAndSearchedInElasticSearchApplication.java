5caea4cda86db079a6e1e0c03846e5ea38f0b4f4
rajadileepkolli <rajadileepkolli@gmail.com>
2021-11-12 18:22:30 +0530

    initial commit of mongodb-elastic-search

---
+++ mongodb-elasticsearch-integration/src/main/java/com/example/mongoes/mongodbelasticsearchintegration/SampleApplicationWhereDataIsSavedInMongoDbAndSearchedInElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongoes.mongodbelasticsearchintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplicationWhereDataIsSavedInMongoDbAndSearchedInElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplicationWhereDataIsSavedInMongoDbAndSearchedInElasticSearchApplication.class, args);
+	}
+
+}
---
+++ mongodb-elasticsearch-integration/src/test/java/com/example/mongoes/mongodbelasticsearchintegration/SampleApplicationWhereDataIsSavedInMongoDbAndSearchedInElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongoes.mongodbelasticsearchintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationWhereDataIsSavedInMongoDbAndSearchedInElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
