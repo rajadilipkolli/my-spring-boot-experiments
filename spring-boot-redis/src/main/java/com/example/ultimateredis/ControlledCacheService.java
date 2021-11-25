22d569db46043a76767bae25f10fcb1cb1d15178
rajadileepkolli <rajadileepkolli@gmail.com>
2021-11-26 00:08:34 +0530

    ultimate spring boot redis cache

---
+++ spring-boot-redis/src/main/java/com/example/ultimateredis/ControlledCacheService.java
@@ -0,0 +1,35 @@
+package com.example.ultimateredis;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ControlledCacheService {
+
+  private static final String CONTROLLED_PREFIX = "myControlledPrefix_";
+
+  public static String getCacheKey(String relevant) {
+    return CONTROLLED_PREFIX + relevant;
+  }
+
+  @Cacheable(
+      cacheNames = "myControlledCache",
+      key = "T(com.example.ultimateredis.ControlledCacheService).getCacheKey(#relevant)")
+  public String getFromCache(String relevant) {
+    return null;
+  }
+
+  @CachePut(
+      cacheNames = "myControlledCache",
+      key = "T(com.example.ultimateredis.ControlledCacheService).getCacheKey(#relevant)")
+  public String populateCache(String relevant, String unrelevantTrackingId) {
+    return "this is it again!";
+  }
+
+  @CacheEvict(
+      cacheNames = "myControlledCache",
+      key = "T(com.example.ultimateredis.ControlledCacheService).getCacheKey(#relevant)")
+  public void removeFromCache(String relevant) {}
+}
---
+++ spring-boot-redis/src/test/java/com/example/ultimateredis/UltimateRedisApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.ultimateredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateRedisApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
