e55d896f64ce99a99fd21204f01b9729a73fc5ef
rajadileepkolli <rajadileepkolli@gmail.com>
2021-12-26 21:47:03 +0530

    initial commit of multiple database

---
+++ multipledatasources/src/main/java/com/example/multipledatasources/MultipledatasourcesApplication.java
@@ -0,0 +1,13 @@
+package com.example.multipledatasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipledatasourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipledatasourcesApplication.class, args);
+	}
+
+}
