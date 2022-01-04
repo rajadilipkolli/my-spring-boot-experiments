40a71428a3b26bc38d7a7a8a142c1f3470755da7
rajadileepkolli <rajadileepkolli@gmail.com>
2022-01-04 13:00:36 +0530

    generated project from spring.io

---
+++ read-replica-postgresql/src/main/java/com/example/demo/readreplica/ReadReplicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.readreplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadReplicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadReplicaApplication.class, args);
+	}
+
+}
