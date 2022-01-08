99872c9bfd34e679ab7c0e1928686c92a4da2ec9
rajadileepkolli <rajadileepkolli@gmail.com>
2022-01-08 22:37:57 +0530

    adds logs to debug issue

--- multipledatasources/src/main/java/com/example/multipledatasources/MultipleDataSourcesApplication.java
+++ multipledatasources/src/main/java/com/example/multipledatasources/MultipleDataSourcesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultipledatasourcesApplication {
+public class MultipleDataSourcesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MultipledatasourcesApplication.class, args);
+		SpringApplication.run(MultipleDataSourcesApplication.class, args);
 	}
 
 }
--- multipledatasources/src/main/java/com/example/multipledatasources/bootstrap/DataInitilizer.java
+++ multipledatasources/src/main/java/com/example/multipledatasources/bootstrap/DataInitilizer.java
@@ -4,37 +4,39 @@ import com.example.multipledatasources.model.cardholder.CardHolder;
 import com.example.multipledatasources.model.member.Member;
 import com.example.multipledatasources.repository.cardholder.CardHolderRepository;
 import com.example.multipledatasources.repository.member.MemberRepository;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class DataInitilizer {
 
-    private final CardHolderRepository cardHolderRepository;
-    private final MemberRepository memberRepository;
-    
-    @EventListener(ApplicationReadyEvent.class)
-    void loadInitialData() {
+  private final CardHolderRepository cardHolderRepository;
+  private final MemberRepository memberRepository;
 
-        String memberId = "1";
+  @EventListener(ApplicationReadyEvent.class)
+  void loadInitialData() {
 
-        Member member =  new Member();
-        member.setMemberId(memberId);
-        member.setName("raja");
+    log.info("Data Boot strapping started");
+    String memberId = "1";
 
-        CardHolder cardHolder = new CardHolder();
-        cardHolder.setMemberId(memberId);
-        cardHolder.setCardNumber("1234-5678-9012-3456");
+    Member member = new Member();
+    member.setMemberId(memberId);
+    member.setName("raja");
 
-        this.cardHolderRepository.save(cardHolder);
-        this.memberRepository.save(member);
-        log.info("Inserted data into both databases");
-    }
+    CardHolder cardHolder = new CardHolder();
+    cardHolder.setMemberId(memberId);
+    cardHolder.setCardNumber("1234-5678-9012-3456");
+
+    Member savedMember = this.memberRepository.save(member);
+    log.info("Saved Member :{}", savedMember);
+
+    CardHolder savedCardHolder = this.cardHolderRepository.save(cardHolder);
+    log.info("Saved CardHolder :{}", savedCardHolder);
+    log.info("Data Boot strapping completed");
+  }
 }
