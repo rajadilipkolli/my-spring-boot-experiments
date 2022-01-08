1597dca62b73bd4f289f9c6f8ecc29881846d2e1
rajadileepkolli <rajadileepkolli@gmail.com>
2022-01-09 00:05:19 +0530

    adds service level junits

---
+++ multipledatasources/src/test/java/com/example/multipledatasources/service/impl/DetailsServiceImplTest.java
@@ -0,0 +1,44 @@
+package com.example.multipledatasources.service.impl;
+
+import com.example.multipledatasources.dto.ResponseDto;
+import com.example.multipledatasources.model.cardholder.CardHolder;
+import com.example.multipledatasources.model.member.Member;
+import com.example.multipledatasources.repository.cardholder.CardHolderRepository;
+import com.example.multipledatasources.repository.member.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class DetailsServiceImplTest {
+
+  @Mock private CardHolderRepository cardHolderRepository;
+
+  @Mock private MemberRepository memberRepository;
+
+  @InjectMocks private DetailsServiceImpl detailsService;
+
+  @Test
+  void getDetails() {
+    // given
+    Member member = new Member();
+    member.setName("junit");
+    given(memberRepository.findByMemberId("1")).willReturn(Optional.of(member));
+
+    given(cardHolderRepository.findByMemberId("1")).willReturn(Optional.of(new CardHolder()));
+    // when
+    ResponseDto responseDto = detailsService.getDetails("1");
+
+    // then
+    assertThat(responseDto.memberId()).isEqualTo("1");
+    assertThat(responseDto.cardNumber()).isNull();
+    assertThat(responseDto.memberName()).isEqualTo("junit");
+  }
+}
