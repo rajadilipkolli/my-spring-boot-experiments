cc3647ced5559e3d59775f28c81405fdc38ae1a9
rajadileepkolli <rajadileepkolli@gmail.com>
2022-01-08 22:19:03 +0530

    fixes correct type of id generation

--- multipledatasources/src/main/java/com/example/multipledatasources/model/cardholder/CardHolder.java
+++ multipledatasources/src/main/java/com/example/multipledatasources/model/cardholder/CardHolder.java
@@ -22,7 +22,7 @@ import java.util.Objects;
 public class CardHolder {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String memberId;
