d0f04775e0ba73ae32ac619ba2a6913defd7a23c
rajadileepkolli <rajadileepkolli@gmail.com>
2022-01-08 22:51:36 +0530

    fixes issue with member table column table

--- multipledatasources/src/main/java/com/example/multipledatasources/model/member/Member.java
+++ multipledatasources/src/main/java/com/example/multipledatasources/model/member/Member.java
@@ -1,18 +1,17 @@
 package com.example.multipledatasources.model.member;
 
-
-import org.hibernate.Hibernate;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import java.util.Objects;
 
 @Entity
@@ -22,24 +21,26 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
-    @Column(nullable = false)
-    private String name;
-    @Column(nullable = false)
-    private String memberId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Member member = (Member) o;
-        return id != null && Objects.equals(id, member.id);
-    }
+  @Column(nullable = false)
+  private String name;
 
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
+  @Column(nullable = false, name = "member_id")
+  private String memberId;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+    Member member = (Member) o;
+    return id != null && Objects.equals(id, member.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }
