af9461539d21eda311e7ce07b6b4779d43402079
rajadileepkolli <rajadileepkolli@gmail.com>
2022-01-27 11:43:18 +0530

    migrate from springfox to springdoc

--- spring-graphql/src/main/java/com/example/graphql/config/SwaggerConfig.java
+++ spring-graphql/src/main/java/com/example/graphql/config/SwaggerConfig.java
@@ -1,37 +1,35 @@
 package com.example.graphql.config;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Collections;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class SwaggerConfig {
+
   @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build()
-        .apiInfo(apiInfo());
+  public GroupedOpenApi publicApi() {
+      return GroupedOpenApi.builder()
+              .group("spring-graphql")
+              .packagesToScan("com.example.graphql.controller")
+              .build();
   }
 
-  private ApiInfo apiInfo() {
-    return new ApiInfo(
-        "My REST API",
-        "Description of API.",
-        "API TOS",
-        "Terms of service",
-        new Contact("Team", "www.example.com", "support@example.com"),
-        "License of API",
-        "API license URL",
-        Collections.emptyList());
+  @Bean
+  public OpenAPI springShopOpenAPI() {
+      return new OpenAPI()
+              .info(new Info().title("SpringGraphQL API")
+              .description("Spring graphql sample application")
+              .version("v0.0.1")
+              .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+              .externalDocs(new ExternalDocumentation()
+              .description("SpringGraphQL Wiki Documentation")
+              .url("https://springshop.wiki.github.org/docs"));
   }
+
 }
