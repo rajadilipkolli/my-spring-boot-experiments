ec097d9ea506fa842a29b7f4ac75008661857ca3
Raja Dilip Kolli <rajadileepkolli@gmail.com>
2022-05-03 06:04:08 +0000

    enables interactive graphql

--- archunit-api/src/test/java/com/example/archunit/architecture/GeneralCodingRulesTest.java
+++ archunit-api/src/test/java/com/example/archunit/architecture/GeneralCodingRulesTest.java
@@ -1,22 +1,15 @@
 package com.example.archunit.architecture;
 
 import static com.example.archunit.architecture.ArchitectureConstants.DEFAULT_PACKAGE;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JODATIME;
 
-import org.springframework.context.annotation.Bean;
-
-import com.tngtech.archunit.core.domain.JavaModifier;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-
-import ch.qos.logback.classic.Logger;
 
 @AnalyzeClasses(packages = DEFAULT_PACKAGE, importOptions = ImportOption.DoNotIncludeTests.class)
 class GeneralCodingRulesTest {
