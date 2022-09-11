3b8d012017ae43ffaee3aee7fddea002b4dfda2b
Raja Kolli <rajadilipkolli@gmail.com>
2022-09-11 07:05:46 +0000

    initial setup of choas monkey

---
+++ my-spring-choas-monkey/src/test/java/com/example/choasmonkey/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.example.choasmonkey;
+
+import com.example.choasmonkey.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
