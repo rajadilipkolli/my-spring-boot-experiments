3a1358e4493cdbc2fecca246567043335895c78a
Raja Kolli <rajadilipkolli@gmail.com>
2022-09-26 17:36:36 +0530

    fixes build issues

--- toggle-spring-boot/src/main/resources/db/migration/h2/V2__create_customers_table.sql
+++ toggle-spring-boot/src/main/resources/db/migration/h2/V2__create_customers_table.sql
@@ -3,5 +3,7 @@ create sequence customer_id_seq start with 1 increment by 100;
 create table customers (
     id bigint DEFAULT nextval('customer_id_seq') not null,
     text varchar(1024) not null,
+    name varchar(1024),
+    zip_code int,
     primary key (id)
 );
--- toggle-spring-boot/src/main/resources/db/migration/mysql/V2__create_customers_table.sql
+++ toggle-spring-boot/src/main/resources/db/migration/mysql/V2__create_customers_table.sql
@@ -1,5 +1,7 @@
 create table customers (
     id bigint not null auto_increment,
     text varchar(1024) not null,
+    name varchar(1024),
+    zip_code int,
     primary key (id)
 );
