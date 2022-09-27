2aceab67f39b5f59625b37df696305305fabb42a
Raja Kolli <rajadilipkolli@gmail.com>
2022-09-27 10:13:52 +0000

    initial commit of mongodb elasticsearch integration

---
+++ spring-boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.example.mongoes;
+
+import com.example.mongoes.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
---
+++ spring-boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/common/ExceptionHandling.java
@@ -0,0 +1,11 @@
+package com.example.mongoes.common;
+
+import static com.example.mongoes.utils.AppConstants.PROFILE_TEST;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.zalando.problem.spring.web.advice.ProblemHandling;
+
+@Profile(PROFILE_TEST)
+@ControllerAdvice
+public final class ExceptionHandling implements ProblemHandling {}
