0d54e8bdff37c92715b3bebc09f028afe0af1302
Raja Kolli <rajadilipkolli@gmail.com>
2022-09-28 13:34:16 +0000

    Initial setup of spring boot jobrunr sample

---
+++ spring-boot-jobrunr-sample/src/main/java/com/example/jobrunr/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.example.jobrunr.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "spring-boot-jobrunr-sample", version = "v1"))
+public class SwaggerConfig {}
---
+++ spring-boot-jobrunr-sample/src/test/java/com/example/jobrunr/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.example.jobrunr;
+
+import com.example.jobrunr.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
