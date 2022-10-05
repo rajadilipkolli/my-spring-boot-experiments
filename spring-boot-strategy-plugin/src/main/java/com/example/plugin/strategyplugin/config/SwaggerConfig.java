29bfe701ebf248ebe3eb25e16f4acdde9a55233f
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-05 11:46:45 +0530

    records shouldn't be used for components

--- spring-boot-strategy-plugin/src/main/java/com/example/plugin/strategyplugin/config/SwaggerConfig.java
+++ spring-boot-strategy-plugin/src/main/java/com/example/plugin/strategyplugin/config/SwaggerConfig.java
@@ -7,5 +7,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "my-spring-envers", version = "v1"), servers = @Server(url = "/"))
+@OpenAPIDefinition(info = @Info(title = "my-spring-envers", version = "v1"), 
+    servers = @Server(url = "/"))
 public class SwaggerConfig {}
--- spring-boot-strategy-plugin/src/main/java/com/example/plugin/strategyplugin/plugin/CSVWriter.java
+++ spring-boot-strategy-plugin/src/main/java/com/example/plugin/strategyplugin/plugin/CSVWriter.java
@@ -1,17 +1,22 @@
 package com.example.plugin.strategyplugin.plugin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 class CSVWriter implements WriterPlugin {
 
-  @Override
-  public String write(String message) {
-    return "Writing CSV " + message;
-  }
+    private static final Logger logger = LoggerFactory.getLogger(CSVWriter.class);
 
-  @Override
-  public boolean supports(String delimiter) {
-    return delimiter.equalsIgnoreCase("csv");
-  }
+    @Override
+    public String write(String message) {
+        logger.info("writing data for type csv with message :{}", message);
+        return "Writing CSV " + message;
+    }
+
+    @Override
+    public boolean supports(String delimiter) {
+        return delimiter.equalsIgnoreCase("csv");
+    }
 }
