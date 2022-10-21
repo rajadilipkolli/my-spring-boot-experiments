28adecdbdb0df6b4e94fc725d2a83f1cc7cc2896
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-21 18:30:47 +0530

    Upgrade to springboot 3.0.0-RC1

--- spring-boot-jpa-multitenancy/schema/src/main/java/com/example/multitenancy/schema/entities/Customer.java
+++ spring-boot-jpa-multitenancy/schema/src/main/java/com/example/multitenancy/schema/entities/Customer.java
@@ -1,13 +1,13 @@
 package com.example.multitenancy.schema.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,8 +22,7 @@ import lombok.Setter;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_generator")
-    @SequenceGenerator(name = "customer_id_generator", sequenceName = "customers_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false)
