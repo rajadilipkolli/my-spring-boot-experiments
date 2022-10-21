21bb5ae8472fc388a060aa0315f7d90576808216
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-21 06:15:51 +0000

    adds swagger for testing in local

--- spring-boot-multipledatasources/src/main/java/com/example/multipledatasources/configuration/SwaggerConfig.java
+++ spring-boot-multipledatasources/src/main/java/com/example/multipledatasources/configuration/SwaggerConfig.java
@@ -1,12 +1,12 @@
-package com.example.plugin.strategyplugin.config;
+package com.example.multipledatasources.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
-
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "spring-boot-strategy-plugin", version = "v1"), 
-    servers = @Server(url = "/"))
+@OpenAPIDefinition(
+        info = @Info(title = "spring-boot-multipledatasources", version = "v1"),
+        servers = @Server(url = "/"))
 public class SwaggerConfig {}
