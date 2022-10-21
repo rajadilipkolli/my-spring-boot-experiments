2b16c808661af08290741a844d405bbad3db1fc6
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-21 20:25:54 +0000

    adds files

--- spring-boot-mongodb-elasticsearch/src/main/java/com/example/mongoes/document/Restaurant.java
+++ spring-boot-mongodb-elasticsearch/src/main/java/com/example/mongoes/document/Restaurant.java
@@ -3,7 +3,7 @@ package com.example.mongoes.document;
 import com.example.mongoes.utils.AppConstants;
 import java.util.ArrayList;
 import java.util.List;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
