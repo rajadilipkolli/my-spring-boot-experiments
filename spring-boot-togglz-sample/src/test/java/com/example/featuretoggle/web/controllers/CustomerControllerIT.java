61a53d34bcc1e9e892965a0fb73e3edbb32bfe23
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-25 20:50:07 +0530

    Revert "upgrade to SpringBoot 3.0.0-RC1" (#346)
    This reverts commit 4ecaa131a53a5df30945895c7ddb5dcefd9732c8.

--- spring-boot-togglz-sample/src/test/java/com/example/featuretoggle/web/controllers/CustomerControllerIT.java
+++ spring-boot-togglz-sample/src/test/java/com/example/featuretoggle/web/controllers/CustomerControllerIT.java
@@ -1,6 +1,7 @@
 package com.example.featuretoggle.web.controllers;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -80,11 +81,15 @@ class CustomerControllerIT extends AbstractIntegrationTest {
                                 .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isBadRequest())
                 .andExpect(header().string("Content-Type", is("application/problem+json")))
-                .andExpect(jsonPath("$.type", is("about:blank")))
-                .andExpect(jsonPath("$.title", is("Bad Request")))
+                .andExpect(
+                        jsonPath(
+                                "$.type",
+                                is("https://zalando.github.io/problem/constraint-violation")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
                 .andExpect(jsonPath("$.status", is(400)))
-                .andExpect(jsonPath("$.detail", is("Invalid request content.")))
-                .andExpect(jsonPath("$.instance", is("/api/customers")))
+                .andExpect(jsonPath("$.violations", hasSize(1)))
+                .andExpect(jsonPath("$.violations[0].field", is("text")))
+                .andExpect(jsonPath("$.violations[0].message", is("Text cannot be empty")))
                 .andReturn();
     }
 
