0ffd3b9931d23277e518d2796dc7ffd108dfdc86
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-25 15:13:06 +0000

    Revert "upgrade to SpringBoot 3.0.0-RC1"
    This reverts commit 4ecaa131a53a5df30945895c7ddb5dcefd9732c8.

--- spring-boot-togglz-sample/src/test/java/com/example/featuretoggle/web/controllers/CustomerControllerTest.java
+++ spring-boot-togglz-sample/src/test/java/com/example/featuretoggle/web/controllers/CustomerControllerTest.java
@@ -3,6 +3,7 @@ package com.example.featuretoggle.web.controllers;
 import static com.example.featuretoggle.utils.AppConstants.PROFILE_TEST;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
@@ -28,6 +29,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.zalando.problem.jackson.ProblemModule;
+import org.zalando.problem.violations.ConstraintViolationProblemModule;
 
 @WebMvcTest(controllers = CustomerController.class)
 @ActiveProfiles(PROFILE_TEST)
@@ -47,6 +50,9 @@ class CustomerControllerTest {
         this.customerList.add(new Customer(1L, "text 1", "name 1", 1));
         this.customerList.add(new Customer(2L, "text 2", "name 2", 2));
         this.customerList.add(new Customer(3L, "text 3", "name 3", 3));
+
+        objectMapper.registerModule(new ProblemModule());
+        objectMapper.registerModule(new ConstraintViolationProblemModule());
     }
 
     @Test
@@ -108,11 +114,15 @@ class CustomerControllerTest {
                                 .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isBadRequest())
                 .andExpect(header().string("Content-Type", is("application/problem+json")))
-                .andExpect(jsonPath("$.type", is("about:blank")))
-                .andExpect(jsonPath("$.title", is("Bad Request")))
+                .andExpect(
+                        jsonPath(
+                                "$.type",
+                                is("https://zalando.github.io/problem/constraint-violation")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
                 .andExpect(jsonPath("$.status", is(400)))
-                .andExpect(jsonPath("$.detail", is("Invalid request content.")))
-                .andExpect(jsonPath("$.instance", is("/api/customers")))
+                .andExpect(jsonPath("$.violations", hasSize(1)))
+                .andExpect(jsonPath("$.violations[0].field", is("text")))
+                .andExpect(jsonPath("$.violations[0].message", is("Text cannot be empty")))
                 .andReturn();
     }
 
