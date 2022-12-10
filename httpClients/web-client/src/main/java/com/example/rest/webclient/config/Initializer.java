07fad19d5d7bda9a836677052219b4a093c84cbb
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-10 08:07:34 +0000

    Implementing Web Client

--- httpClients/web-client/src/main/java/com/example/rest/webclient/config/Initializer.java
+++ httpClients/web-client/src/main/java/com/example/rest/webclient/config/Initializer.java
@@ -1,19 +1,35 @@
 package com.example.rest.webclient.config;
 
+import com.example.rest.webclient.entities.Post;
+import com.example.rest.webclient.repositories.PostRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class Initializer implements CommandLineRunner {
 
-    private final ApplicationProperties properties;
+    private final PostRepository postRepository;
+    private final WebClient webClient;
 
     @Override
     public void run(String... args) {
         log.info("Running Initializer.....");
+        List<Post> response =
+                webClient
+                        .get()
+                        .uri("/posts")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .retrieve()
+                        .bodyToMono(new ParameterizedTypeReference<List<Post>>() {})
+                        .block();
+        this.postRepository.saveAll(response);
     }
 }
