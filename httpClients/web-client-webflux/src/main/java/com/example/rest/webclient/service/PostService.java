3d5854c5fc2dfc7ed499e4e898a236614b7e54ad
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-10 13:50:11 +0000

    adds code to save and delete object

--- httpClients/web-client-webflux/src/main/java/com/example/rest/webclient/service/PostService.java
+++ httpClients/web-client-webflux/src/main/java/com/example/rest/webclient/service/PostService.java
@@ -1,6 +1,7 @@
 package com.example.rest.webclient.service;
 
 import com.example.rest.webclient.model.Post;
+import java.util.Comparator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,16 +15,31 @@ public class PostService {
     private final WebClient webClient;
 
     public Flux<Post> findAllPosts(String sortBy, String sortDir) {
-        return webClient.get().uri("/posts").retrieve().bodyToFlux(Post.class);
+        return webClient
+                .get()
+                .uri("/posts")
+                .retrieve()
+                .bodyToFlux(Post.class)
+                .sort(Comparator.comparing(Post::id));
     }
 
     public Mono<Post> findPostById(Long id) {
-        return webClient.get().uri("/posts/" + id).retrieve().bodyToMono(Post.class);
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/posts/{postId}").build(id))
+                .retrieve()
+                .bodyToMono(Post.class);
     }
 
     public Mono<Post> savePost(Post post) {
-        return null;
+        return webClient.post().uri("/posts").retrieve().bodyToMono(Post.class);
     }
 
-    public void deletePostById(Long id) {}
+    public Mono<Post> deletePostById(Long id) {
+        return webClient
+                .delete()
+                .uri(uriBuilder -> uriBuilder.path("/posts/{postId}").build(id))
+                .retrieve()
+                .bodyToMono(Post.class);
+    }
 }
