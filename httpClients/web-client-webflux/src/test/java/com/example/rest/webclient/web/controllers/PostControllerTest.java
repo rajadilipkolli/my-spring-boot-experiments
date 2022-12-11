041733431151aa80f2293cf1881d4d45bb8cde30
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-11 11:58:48 +0000

    adds Junits

---
+++ httpClients/web-client-webflux/src/test/java/com/example/rest/webclient/web/controllers/PostControllerTest.java
@@ -0,0 +1,209 @@
+package com.example.rest.webclient.web.controllers;
+
+import static com.example.rest.webclient.utils.AppConstants.PROFILE_TEST;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.example.rest.webclient.model.Post;
+import com.example.rest.webclient.service.PostService;
+import com.example.rest.webclient.web.controller.PostController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest(controllers = PostController.class)
+@ActiveProfiles(PROFILE_TEST)
+class PostControllerTest {
+
+    @Autowired private WebTestClient webTestClient;
+
+    @MockBean private PostService postService;
+
+    @Autowired private ObjectMapper objectMapper;
+
+    private List<Post> postList;
+
+    @BeforeEach
+    void setUp() {
+        this.postList = new ArrayList<>();
+        this.postList.add(new Post(1L, "text 1", 1L, "First Body"));
+        this.postList.add(new Post(2L, "text 2", 1L, "Second Body"));
+        this.postList.add(new Post(3L, "text 3", 1L, "Third Body"));
+    }
+
+    @Test
+    void shouldFetchAllPosts() {
+        given(postService.findAllPosts("id", "asc")).willReturn(Flux.fromIterable(postList));
+
+        this.webTestClient
+                .get()
+                .uri("/api/posts")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(Post.class)
+                .hasSize(3);
+    }
+
+    @Test
+    void shouldFindPostById() {
+        Long postId = 1L;
+        Post post = new Post(postId, "text 1", 1L, "First Body");
+        given(postService.findPostById(postId)).willReturn(Mono.just(post));
+
+        this.webTestClient
+                .get()
+                .uri("/api/posts/{id}", postId)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Post.class)
+                .value(Post::title, is(post.title()));
+    }
+
+    @Test
+    void shouldReturn404WhenFetchingNonExistingPost() {
+        Long postId = 1L;
+        given(postService.findPostById(postId)).willReturn(Mono.empty());
+
+        this.webTestClient
+                .get()
+                .uri("/api/posts/{id}", postId)
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
+
+    @Test
+    @Disabled
+    void shouldCreateNewPost() {
+        given(postService.savePost(any(Post.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        Post post = new Post(1L, "some text", 1L, "First Body");
+        this.webTestClient
+                .post()
+                .uri("/api/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(post))
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(Post.class)
+                .value(Post::id, notNullValue())
+                .value(Post::title, is(post.title()));
+    }
+
+    @Test
+    @Disabled
+    void shouldReturn400WhenCreateNewPostWithoutText() throws Exception {
+        Post post = new Post(null, null, null, null);
+
+        this.webTestClient
+                .post()
+                .uri("/api/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(post, Post.class)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(ProblemDetail.class)
+                .value(ProblemDetail::getType, is("about:blank"))
+                .value(ProblemDetail::getTitle, is("Constraint Violation"))
+                .value(ProblemDetail::getStatus, is("400"))
+                .value(ProblemDetail::getDetail, is("Invalid request content."))
+                .value(ProblemDetail::getInstance, is("/api/posts"))
+                .consumeWith(
+                        problemDetailEntityExchangeResult -> {
+                            assertThat(
+                                            problemDetailEntityExchangeResult
+                                                    .getRequestHeaders()
+                                                    .get(HttpHeaders.CONTENT_TYPE))
+                                    .containsExactly(MediaType.APPLICATION_PROBLEM_JSON_VALUE);
+                        });
+    }
+
+    @Test
+    @Disabled
+    void shouldUpdatePost() throws Exception {
+        Long postId = 1L;
+        Post post = new Post(postId, "Updated text", 1L, "First Body");
+        given(postService.findPostById(postId)).willReturn(Mono.just(post));
+        given(postService.savePost(any(Post.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        this.webTestClient
+                .put()
+                .uri("/api/posts/{id}", postId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(objectMapper.writeValueAsString(post))
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Post.class)
+                .value(Post::title, is(post.title()));
+    }
+
+    @Test
+    void shouldReturn404WhenUpdatingNonExistingPost() throws Exception {
+        Long postId = 1L;
+        given(postService.findPostById(postId)).willReturn(Mono.empty());
+        Post post = new Post(postId, "Updated text", 1L, "First Body");
+
+        this.webTestClient
+                .put()
+                .uri("/api/posts/{id}", postId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(objectMapper.writeValueAsString(post))
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
+
+    @Test
+    void shouldDeletePost() {
+        Long postId = 1L;
+        Post post = new Post(postId, "Some text", 1L, "First Body");
+        given(postService.findPostById(postId)).willReturn(Mono.just(post));
+        given(postService.deletePostById(post.id())).willReturn(Mono.just(post));
+
+        this.webTestClient
+                .delete()
+                .uri("/api/posts/{id}", postId)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(Post.class)
+                .value(Post::title, is(post.title()));
+    }
+
+    @Test
+    void shouldReturn404WhenDeletingNonExistingPost() {
+        Long postId = 1L;
+        given(postService.findPostById(postId)).willReturn(Mono.empty());
+
+        this.webTestClient
+                .delete()
+                .uri("/api/posts/{id}", postId)
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
+}
