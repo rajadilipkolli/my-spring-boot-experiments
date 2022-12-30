929baa19987244d28944ca7d88d6fc5a5c6faddf
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-30 18:08:13 +0000

    Setting MariaDb as secondary db

--- jpa/spring-boot-jpa-multitenancy/multidatasource-multitenancy/src/test/java/com/example/multitenancy/common/DBContainerInitializer.java
+++ jpa/spring-boot-jpa-multitenancy/multidatasource-multitenancy/src/test/java/com/example/multitenancy/common/DBContainerInitializer.java
@@ -3,36 +3,37 @@ package com.example.multitenancy.common;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.lifecycle.Startables;
 
 public class DBContainerInitializer
         implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    private static final PostgreSQLContainer<?> sqlContainer =
+    private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER =
             new PostgreSQLContainer<>("postgres:15-alpine")
                     .withDatabaseName("integration-tests-db")
                     .withUsername("username")
                     .withPassword("password");
 
-    private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER =
-            new PostgreSQLContainer<>("postgres:15-alpine")
-                    .withDatabaseName("integration-tests")
+    private static final MariaDBContainer<?> MARIA_DB_CONTAINER =
+            new MariaDBContainer<>("mariadb:10.10")
+                    .withDatabaseName("integration-tests-db")
                     .withUsername("username")
                     .withPassword("password");
 
     static {
-        Startables.deepStart(sqlContainer, POSTGRE_SQL_CONTAINER).join();
+        Startables.deepStart(POSTGRESQL_CONTAINER, MARIA_DB_CONTAINER).join();
     }
 
     public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
         TestPropertyValues.of(
-                        "datasource.primary.url=" + sqlContainer.getJdbcUrl(),
-                        "datasource.primary.username=" + sqlContainer.getUsername(),
-                        "datasource.primary.password=" + sqlContainer.getPassword(),
-                        "datasource.secondary.url=" + POSTGRE_SQL_CONTAINER.getJdbcUrl(),
-                        "datasource.secondary.username=" + POSTGRE_SQL_CONTAINER.getUsername(),
-                        "datasource.secondary.password=" + POSTGRE_SQL_CONTAINER.getPassword())
+                        "datasource.primary.url=" + POSTGRESQL_CONTAINER.getJdbcUrl(),
+                        "datasource.primary.username=" + POSTGRESQL_CONTAINER.getUsername(),
+                        "datasource.primary.password=" + POSTGRESQL_CONTAINER.getPassword(),
+                        "datasource.secondary.url=" + MARIA_DB_CONTAINER.getJdbcUrl(),
+                        "datasource.secondary.username=" + MARIA_DB_CONTAINER.getUsername(),
+                        "datasource.secondary.password=" + MARIA_DB_CONTAINER.getPassword())
                 .applyTo(configurableApplicationContext.getEnvironment());
     }
 }
