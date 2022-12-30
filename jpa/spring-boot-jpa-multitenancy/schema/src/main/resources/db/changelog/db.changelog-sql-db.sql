505fab489623214050766c7991be374e7c18be64
Raja Kolli <rajadilipkolli@gmail.com>
2022-12-31 00:36:47 +0530

    Polish Creating Schema

--- jpa/spring-boot-jpa-multitenancy/schema/src/main/resources/db/changelog/db.changelog-sql-db.sql
+++
@@ -1,8 +0,0 @@
-create schema if not exists test1;
-create schema if not exists test2;
-
-create sequence test1.customers_seq start with 1 increment by 50;
-create table test1.customers (id bigint not null, name varchar(255), primary key (id));
-
-create sequence test2.customers_seq start with 1 increment by 50;
-create table test2.customers (id bigint not null, name varchar(255), primary key (id));
