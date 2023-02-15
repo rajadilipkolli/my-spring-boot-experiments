b2331b938173f92bd0877d4095c3ca6f73833c38
Raja Kolli <rajadilipkolli@gmail.com>
2023-02-15 10:23:19 +0530

    adds missing utility methods

--- jpa/spring-boot-read-replica-postgresql/src/main/java/com/example/demo/readreplica/entities/Article.java
+++ jpa/spring-boot-read-replica-postgresql/src/main/java/com/example/demo/readreplica/entities/Article.java
@@ -1,6 +1,13 @@
 package com.example.demo.readreplica.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +20,7 @@ import lombok.Setter;
 @Table(name = "articles")
 public class Article {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "article_gen")
-    @SequenceGenerator(name = "article_gen", sequenceName = "articles_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Integer id;
 
@@ -28,4 +34,14 @@ public class Article {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "article", orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+        comment.setArticle(this);
+    }
+
+    public void removeComment(Comment comment) {
+        this.comments.remove(comment);
+        comment.setArticle(null);
+    }
 }
--- jpa/spring-boot-read-replica-postgresql/src/main/java/com/example/demo/readreplica/entities/Comment.java
+++ jpa/spring-boot-read-replica-postgresql/src/main/java/com/example/demo/readreplica/entities/Comment.java
@@ -1,6 +1,13 @@
 package com.example.demo.readreplica.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +18,7 @@ import lombok.Setter;
 public class Comment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comment_gen")
-    @SequenceGenerator(name = "comment_gen", sequenceName = "comment_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Integer id;
 
