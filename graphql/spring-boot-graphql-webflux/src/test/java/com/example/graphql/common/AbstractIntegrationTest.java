1eabd1720546472c37266961876301e28214cde6
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-04 13:28:55 +0000

    Upgrade to SpringBoot 3.1.0-M2

--- graphql/spring-boot-graphql-webflux/src/test/java/com/example/graphql/common/AbstractIntegrationTest.java
+++ graphql/spring-boot-graphql-webflux/src/test/java/com/example/graphql/common/AbstractIntegrationTest.java
@@ -5,12 +5,25 @@ import static com.example.graphql.utils.AppConstants.PROFILE_TEST;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcServiceConnection;
+import org.springframework.boot.test.autoconfigure.r2dbc.R2dbcServiceConnection;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles({PROFILE_TEST, PROFILE_IT})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public abstract class AbstractIntegrationTest extends DBContainerInitializerBase {
+@Testcontainers(disabledWithoutDocker = true)
+public abstract class AbstractIntegrationTest {
 
     @Autowired protected ObjectMapper objectMapper;
+
+    @Container @JdbcServiceConnection @R2dbcServiceConnection
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:15-alpine")
+                    .withDatabaseName("integration-tests-db")
+                    .withUsername("username")
+                    .withPassword("password");
 }
