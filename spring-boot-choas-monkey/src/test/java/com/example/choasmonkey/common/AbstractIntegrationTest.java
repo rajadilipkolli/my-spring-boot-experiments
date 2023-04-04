73af82f477d8321909675a0425c367cd8a55271b
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-04 13:53:49 +0000

    Upgrade to SpringBoot 3.1.0-M2

--- spring-boot-choas-monkey/src/test/java/com/example/choasmonkey/common/AbstractIntegrationTest.java
+++ spring-boot-choas-monkey/src/test/java/com/example/choasmonkey/common/AbstractIntegrationTest.java
@@ -5,19 +5,29 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcServiceConnection;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles({PROFILE_TEST})
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@ContextConfiguration(initializers = {DBContainerInitializer.class})
+@Testcontainers(disabledWithoutDocker = true)
 @AutoConfigureMockMvc
 public abstract class AbstractIntegrationTest {
 
     @Autowired protected MockMvc mockMvc;
 
     @Autowired protected ObjectMapper objectMapper;
+
+    @Container @JdbcServiceConnection
+    protected static final PostgreSQLContainer<?> sqlContainer =
+            new PostgreSQLContainer<>("postgres:15-alpine")
+                    .withDatabaseName("integration-tests-db")
+                    .withUsername("username")
+                    .withPassword("password");
 }
