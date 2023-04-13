69ad0a44c7ad77502aac06947bf5935dfb71b94a
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-13 18:16:51 +0000

    adds Junit test case

---
+++ open-api-spring-boot/src/test/java/org/example/openapi/OpenApiApplicationTest.java
@@ -0,0 +1,24 @@
+package org.example.openapi;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OpenApiApplicationTest {
+
+    @Autowired protected MockMvc mockMvc;
+
+    @Test
+    public void whenReadAll_thenStatusIsNotImplemented() throws Exception {
+        this.mockMvc.perform(get("/api/customers"))
+            .andExpect(status().isNotImplemented());
+    }
+
+}
