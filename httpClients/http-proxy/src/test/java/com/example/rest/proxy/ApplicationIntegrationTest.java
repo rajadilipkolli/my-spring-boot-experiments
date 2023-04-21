d041bf0f32e05f36165e5aa52275ffc9de71d7e4
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-21 14:15:21 +0000

    using jdbcserviceconnection #598

--- httpClients/http-proxy/src/test/java/com/example/rest/proxy/ApplicationIntegrationTest.java
+++ httpClients/http-proxy/src/test/java/com/example/rest/proxy/ApplicationIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.example.rest.proxy;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.example.rest.proxy.common.AbstractIntegrationTest;
 
 import org.junit.jupiter.api.Test;
@@ -7,5 +9,7 @@ import org.junit.jupiter.api.Test;
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        assertThat(sqlContainer.isRunning()).isTrue();
+    }
 }
