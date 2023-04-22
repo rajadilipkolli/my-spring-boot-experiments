f3d26cfd9788828700df7109267e8134c49b96a2
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-22 13:31:00 +0000

    #598 - fixes issue with new serviceConnection

--- graphql/spring-boot-graphql-webflux/src/test/java/com/example/graphql/common/AbstractIntegrationTest.java
+++ graphql/spring-boot-graphql-webflux/src/test/java/com/example/graphql/common/AbstractIntegrationTest.java
@@ -5,9 +5,8 @@ import static com.example.graphql.utils.AppConstants.PROFILE_TEST;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcServiceConnection;
-import org.springframework.boot.test.autoconfigure.r2dbc.R2dbcServiceConnection;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -20,7 +19,7 @@ public abstract class AbstractIntegrationTest {
 
     @Autowired protected ObjectMapper objectMapper;
 
-    @Container @JdbcServiceConnection @R2dbcServiceConnection
+    @Container @ServiceConnection
     protected static final PostgreSQLContainer<?> postgreSQLContainer =
             new PostgreSQLContainer<>("postgres:15-alpine")
                     .withDatabaseName("integration-tests-db")
