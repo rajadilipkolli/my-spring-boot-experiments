5d8e5f94f748cc877713587850f10f73cea5d355
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-22 19:15:29 +0530

    Use Testcontainers new Dev Mode (#625)
    * Update spring boot to v3.1.0-RC1
    
    * Implement testcontainers changes
    
    * #598 - fixes issue with new serviceConnection
    
    * Using new testcontiners-dev mode
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

---
+++ spring-boot-choas-monkey/src/test/java/com/example/choasmonkey/TestcontainersConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.choasmonkey;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:15.2-alpine");
+    }
+}
