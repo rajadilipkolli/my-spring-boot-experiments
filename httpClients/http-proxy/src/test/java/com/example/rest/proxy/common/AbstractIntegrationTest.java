d20d0bb3a61009f5ebaeb6360472c4bc7c9806a0
Raja Kolli <rajadilipkolli@gmail.com>
2023-04-23 04:55:28 +0000

    Implement TC Live mode

--- httpClients/http-proxy/src/test/java/com/example/rest/proxy/common/AbstractIntegrationTest.java
+++ httpClients/http-proxy/src/test/java/com/example/rest/proxy/common/AbstractIntegrationTest.java
@@ -4,30 +4,21 @@ import static com.example.rest.proxy.utils.AppConstants.PROFILE_TEST;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.example.rest.proxy.TestcontainersConfiguration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles({PROFILE_TEST})
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@Testcontainers(disabledWithoutDocker = true)
+@Import(TestcontainersConfiguration.class)
 @AutoConfigureMockMvc
 public abstract class AbstractIntegrationTest {
-
-    @Container @ServiceConnection
-    protected static final PostgreSQLContainer<?> sqlContainer =
-            new PostgreSQLContainer<>("postgres:15-alpine")
-                    .withDatabaseName("integration-tests-db")
-                    .withUsername("username")
-                    .withPassword("password");
 
     @Autowired protected MockMvc mockMvc;
 
--- spring-boot-choas-monkey/src/test/java/com/example/choasmonkey/common/AbstractIntegrationTest.java
+++ spring-boot-choas-monkey/src/test/java/com/example/choasmonkey/common/AbstractIntegrationTest.java
@@ -4,32 +4,23 @@ import static com.example.choasmonkey.utils.AppConstants.PROFILE_TEST;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.example.choasmonkey.TestcontainersConfiguration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles({PROFILE_TEST})
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@Testcontainers(disabledWithoutDocker = true)
+@Import(TestcontainersConfiguration.class)
 @AutoConfigureMockMvc
 public abstract class AbstractIntegrationTest {
 
     @Autowired protected MockMvc mockMvc;
 
     @Autowired protected ObjectMapper objectMapper;
-
-    @Container @ServiceConnection
-    protected static final PostgreSQLContainer<?> sqlContainer =
-            new PostgreSQLContainer<>("postgres:15-alpine")
-                    .withDatabaseName("integration-tests-db")
-                    .withUsername("username")
-                    .withPassword("password");
 }
