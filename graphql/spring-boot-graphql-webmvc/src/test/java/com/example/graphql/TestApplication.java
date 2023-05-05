c5766b764fa78e741f563798f0043ab968228e83
Raja Kolli <rajadilipkolli@gmail.com>
2023-05-05 16:06:40 +0000

    rewrite containers loading

--- graphql/spring-boot-graphql-webmvc/src/test/java/com/example/graphql/TestApplication.java
+++ graphql/spring-boot-graphql-webmvc/src/test/java/com/example/graphql/TestApplication.java
@@ -1,5 +1,12 @@
 package com.example.graphql;
 
+import com.example.graphql.common.MyContainersConfiguration;
+
+import org.springframework.boot.SpringApplication;
+
 public class TestApplication {
-    
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(MyContainersConfiguration.class).run(args);
+    }
 }
--- graphql/spring-boot-graphql-webmvc/src/test/java/com/example/graphql/common/AbstractIntegrationTest.java
+++ graphql/spring-boot-graphql-webmvc/src/test/java/com/example/graphql/common/AbstractIntegrationTest.java
@@ -9,27 +9,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles({PROFILE_TEST})
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@Testcontainers(disabledWithoutDocker = true)
 @AutoConfigureMockMvc
+@Import(MyContainersConfiguration.class)
 public abstract class AbstractIntegrationTest {
 
     @Autowired protected MockMvc mockMvc;
 
     @Autowired protected ObjectMapper objectMapper;
-
-    @Container @ServiceConnection
-    private static final PostgreSQLContainer<?> sqlContainer =
-            new PostgreSQLContainer<>("postgres:15-alpine")
-                    .withDatabaseName("integration-tests-db")
-                    .withUsername("username")
-                    .withPassword("password");
 }
