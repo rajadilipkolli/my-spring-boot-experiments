6082758e4dc2a5e7a06f023ea83cf69225b7672e
Raja Kolli <rajadilipkolli@gmail.com>
2023-05-22 16:13:53 +0000

    using testcontainers for connecting to rabbitMQ

--- spring-boot-rabbitmq-thymeleaf/src/test/java/com/poc/boot/rabbitmq/RabbitMQIntegrationTest.java
+++ spring-boot-rabbitmq-thymeleaf/src/test/java/com/poc/boot/rabbitmq/RabbitMQIntegrationTest.java
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class RabbitMQApplicationTest {
+@Testcontainers(disabledWithoutDocker = true)
+class RabbitMQIntegrationTest {
 
     @Autowired private RabbitTemplate rabbitTemplate;
+
+    @Container
+    @ServiceConnection
+    static final RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3.11.16-management");
 
     @Test
     void contextLoads() {
