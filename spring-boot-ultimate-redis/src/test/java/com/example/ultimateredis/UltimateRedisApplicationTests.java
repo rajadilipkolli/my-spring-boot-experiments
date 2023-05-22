811efab5f637f38866415255c9c5aa508e038ff1
Raja Kolli <rajadilipkolli@gmail.com>
2023-05-22 16:38:09 +0000

    Using testcontainers for connecting to redis

--- spring-boot-ultimate-redis/src/test/java/com/example/ultimateredis/UltimateRedisApplicationTests.java
+++ spring-boot-ultimate-redis/src/test/java/com/example/ultimateredis/UltimateRedisApplicationTests.java
@@ -2,9 +2,18 @@ package com.example.ultimateredis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class UltimateRedisApplicationTests {
+
+    @Container
+    @ServiceConnection(name = "redis")
+    static final GenericContainer redisContainer = new GenericContainer("redis:7.0.11");
 
     @Test
     void contextLoads() {}
