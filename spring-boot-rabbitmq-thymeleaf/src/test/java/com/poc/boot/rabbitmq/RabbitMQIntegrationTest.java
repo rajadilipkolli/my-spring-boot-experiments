728fb92dfe617110c23895edcac5216a13f78797
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-01 06:49:54 +0000

    adds test to verify sending Order

--- spring-boot-rabbitmq-thymeleaf/src/test/java/com/poc/boot/rabbitmq/RabbitMQIntegrationTest.java
+++ spring-boot-rabbitmq-thymeleaf/src/test/java/com/poc/boot/rabbitmq/RabbitMQIntegrationTest.java
@@ -1,22 +1,44 @@
 package com.poc.boot.rabbitmq;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poc.boot.rabbitmq.config.MyTestContainersConfiguration;
+import com.poc.boot.rabbitmq.model.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(MyTestContainersConfiguration.class)
+@AutoConfigureMockMvc
 class RabbitMQIntegrationTest {
 
+    @Autowired private MockMvc mockMvc;
+
     @Autowired private RabbitTemplate rabbitTemplate;
+
+    @Autowired private ObjectMapper objectMapper;
 
     @Test
     void contextLoads() {
         assertThat(this.rabbitTemplate).isNotNull();
+    }
+
+    @Test
+    void testSendingMessage() throws Exception {
+        Order order = new Order();
+        order.setOrderNumber("1");
+        order.setAmount(10d);
+        order.setProductId("P1");
+        this.mockMvc
+                .perform(post("/sendMsg").content(objectMapper.writeValueAsString(order)))
+                .andExpect(status().isFound());
     }
 }
