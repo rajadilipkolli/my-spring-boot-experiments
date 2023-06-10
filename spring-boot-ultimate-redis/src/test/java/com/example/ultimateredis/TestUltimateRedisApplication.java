1c406069d426be51a3cb8aaada38ae3a54d0b7db
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-10 14:48:16 +0000

    Using Test containers in dev mode

---
+++ spring-boot-ultimate-redis/src/test/java/com/example/ultimateredis/TestUltimateRedisApplication.java
@@ -0,0 +1,25 @@
+package com.example.ultimateredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestUltimateRedisApplication {
+
+    @Bean
+    @ServiceConnection(name = "redis")
+    GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis").withTag("7.0.11"))
+                .withExposedPorts(6379);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(UltimateRedisApplication::main)
+                .with(TestUltimateRedisApplication.class)
+                .run(args);
+    }
+}
--- spring-boot-ultimate-redis/src/test/java/com/example/ultimateredis/UltimateRedisApplicationTests.java
+++ spring-boot-ultimate-redis/src/test/java/com/example/ultimateredis/UltimateRedisApplicationTests.java
@@ -2,20 +2,11 @@ package com.example.ultimateredis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Testcontainers
+@Import(TestUltimateRedisApplication.class)
 class UltimateRedisApplicationTests {
-
-    @Container
-    @ServiceConnection(name = "redis")
-    static final GenericContainer redisContainer =
-            new GenericContainer(DockerImageName.parse("redis").withTag("7.0.11"));
 
     @Test
     void contextLoads() {}
