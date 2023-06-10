c555226a2b7d64dd7706d93dbb34c03190ecd1c7
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-10 13:52:38 +0000

    Fixes Junit failures

--- httpClients/rest-template/src/test/java/com/example/rest/template/common/MyContainer.java
+++ httpClients/rest-template/src/test/java/com/example/rest/template/common/MyContainer.java
@@ -1,11 +1,12 @@
 package com.example.rest.template.common;
 
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 public interface MyContainer {
 
-    @Container
+    @Container @ServiceConnection
     static final PostgreSQLContainer<?> sqlContainer =
             new PostgreSQLContainer<>("postgres:15.3-alpine");
 }
