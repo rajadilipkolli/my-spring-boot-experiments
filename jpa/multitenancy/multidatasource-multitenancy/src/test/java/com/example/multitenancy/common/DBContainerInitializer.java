a295ebf5b7572da0147cb05e557d91f6381902d9
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-17 03:41:57 +0000

    adds mariadb as secondary db

--- jpa/multitenancy/multidatasource-multitenancy/src/test/java/com/example/multitenancy/common/DBContainerInitializer.java
+++ jpa/multitenancy/multidatasource-multitenancy/src/test/java/com/example/multitenancy/common/DBContainerInitializer.java
@@ -3,6 +3,7 @@ package com.example.multitenancy.common;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.lifecycle.Startables;
 
@@ -15,14 +16,14 @@ public class DBContainerInitializer
                     .withUsername("username")
                     .withPassword("password");
 
-    private static final PostgreSQLContainer<?> SECONDARY_POSTGRE_SQL_CONTAINER =
-            new PostgreSQLContainer<>("postgres:15.3-alpine")
-                    .withDatabaseName("integration-tests")
+    private static final MariaDBContainer<?> MARIA_DB_CONTAINER =
+            new MariaDBContainer<>("mariadb:11.0")
+                    .withDatabaseName("integration-tests-db")
                     .withUsername("username")
                     .withPassword("password");
 
     static {
-        Startables.deepStart(PRIMARY_POSTGRE_SQL_CONTAINER, SECONDARY_POSTGRE_SQL_CONTAINER).join();
+        Startables.deepStart(PRIMARY_POSTGRE_SQL_CONTAINER, MARIA_DB_CONTAINER).join();
     }
 
     public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
@@ -32,11 +33,9 @@ public class DBContainerInitializer
                                 + PRIMARY_POSTGRE_SQL_CONTAINER.getUsername(),
                         "datasource.primary.password="
                                 + PRIMARY_POSTGRE_SQL_CONTAINER.getPassword(),
-                        "datasource.secondary.url=" + SECONDARY_POSTGRE_SQL_CONTAINER.getJdbcUrl(),
-                        "datasource.secondary.username="
-                                + SECONDARY_POSTGRE_SQL_CONTAINER.getUsername(),
-                        "datasource.secondary.password="
-                                + SECONDARY_POSTGRE_SQL_CONTAINER.getPassword())
+                        "datasource.secondary.url=" + MARIA_DB_CONTAINER.getJdbcUrl(),
+                        "datasource.secondary.username=" + MARIA_DB_CONTAINER.getUsername(),
+                        "datasource.secondary.password=" + MARIA_DB_CONTAINER.getPassword())
                 .applyTo(configurableApplicationContext.getEnvironment());
     }
 }
