6999389d438fd654127eb30cc4ef8055f2501352
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-29 12:28:37 +0000

    polish configuration

--- spring-boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/common/ElasticsearchContainerSetUp.java
+++ spring-boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/common/ElasticsearchContainerSetUp.java
@@ -1,6 +1,5 @@
 package com.example.mongoes.common;
 
-import java.util.Map;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
@@ -9,13 +8,7 @@ import org.testcontainers.lifecycle.Startables;
 public class ElasticsearchContainerSetUp {
 
     protected static final ElasticsearchContainer ELASTICSEARCH_CONTAINER =
-            new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:8.7.1")
-                    .withEnv(
-                            Map.of(
-                                    "discovery.type",
-                                    "single-node",
-                                    "xpack.security.enabled",
-                                    "false"))
+            new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:8.8.2")
                     .withReuse(true);
 
     static {
