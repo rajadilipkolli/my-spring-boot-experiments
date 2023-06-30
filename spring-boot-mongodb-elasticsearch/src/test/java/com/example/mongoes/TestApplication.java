d2c1e77e9f64d3b6a97a96d2387ca3ca7ec71db5
rajadilipkolli <rajadilipkolli@gmail.com>
2023-06-30 19:53:34 +0530

    chore : clean code

--- spring-boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/TestApplication.java
+++ spring-boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/TestApplication.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Map;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ public class TestApplication {
 
     @ServiceConnection
     @Bean
+    @RestartScope
     ElasticsearchContainer elasticsearchContainer() {
         return new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:8.8.2")
                 .withEnv(Map.of("xpack.security.enabled", "false"));
@@ -23,8 +25,9 @@ public class TestApplication {
 
     @ServiceConnection
     @Bean
+    @RestartScope
     MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("6.0.6"));
+        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("6.0.6")).withSharding();
     }
 
     public static void main(String[] args) {
