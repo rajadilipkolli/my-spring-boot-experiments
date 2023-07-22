2544e1f1a471555d1237fcfceb64c0c7174d4c98
Raja Kolli <rajadilipkolli@gmail.com>
2023-07-22 17:40:27 +0000

    chore(fix): fixes Build Issue

--- httpClients/boot-restclient/src/test/java/com/example/restclient/bootrestclient/BootRestclientApplicationTests.java
+++ httpClients/boot-restclient/src/test/java/com/example/restclient/bootrestclient/BootRestclientApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.restclient.bootrestclient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestBootRestclientApplication.class)
 class BootRestclientApplicationTests {
 
 	@Test
