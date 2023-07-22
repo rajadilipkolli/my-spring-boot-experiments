e55affc29aa4ea2c61d1fcab011b71832b5a7c6d
rajadilipkolli <rajadilipkolli@gmail.com>
2023-07-22 21:27:34 +0530

    feat: Initial commit of Spring boot restclient

---
+++ httpClients/boot-restclient/src/main/java/com/example/restclient/bootrestclient/BootRestclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.restclient.bootrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestclientApplication.class, args);
+	}
+
+}
---
+++ httpClients/boot-restclient/src/test/java/com/example/restclient/bootrestclient/BootRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restclient.bootrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
