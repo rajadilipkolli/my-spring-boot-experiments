cb39067b72020e8bf0c97489c939cdd130fb621f
rajadilipkolli <rajadilipkolli@gmail.com>
2023-08-05 23:43:28 +0530

    feat: polish

--- boot-choas-monkey/src/test/java/com/example/choasmonkey/TestApplication.java
+++ boot-choas-monkey/src/test/java/com/example/choasmonkey/TestApplication.java
@@ -1,11 +1,23 @@
 package com.example.choasmonkey;
 
-import com.example.choasmonkey.config.TestcontainersConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
+@TestConfiguration(proxyBeanMethods = false)
 public class TestApplication {
 
+    @Bean
+    @ServiceConnection
+    @RestartScope
+    public PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:15.3-alpine");
+    }
+
     public static void main(String[] args) {
-        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(Application::main).with(TestApplication.class).run(args);
     }
 }
--- boot-choas-monkey/src/test/java/com/example/choasmonkey/common/AbstractIntegrationTest.java
+++ boot-choas-monkey/src/test/java/com/example/choasmonkey/common/AbstractIntegrationTest.java
@@ -3,18 +3,16 @@ package com.example.choasmonkey.common;
 import static com.example.choasmonkey.utils.AppConstants.PROFILE_TEST;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import com.example.choasmonkey.config.TestcontainersConfiguration;
+import com.example.choasmonkey.TestApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles({PROFILE_TEST})
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = TestApplication.class)
 @AutoConfigureMockMvc
 public abstract class AbstractIntegrationTest {
 
