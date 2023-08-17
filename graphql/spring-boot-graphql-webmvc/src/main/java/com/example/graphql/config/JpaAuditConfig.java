de2a4b1af7d7d67db89c9a487d83360a99ffd594
Raja Kolli <rajadilipkolli@gmail.com>
2023-08-17 04:12:41 +0000

    polish : jpa properties

--- graphql/spring-boot-graphql-webmvc/src/main/java/com/example/graphql/config/JpaAuditConfig.java
+++ graphql/spring-boot-graphql-webmvc/src/main/java/com/example/graphql/config/JpaAuditConfig.java
@@ -3,6 +3,7 @@ package com.example.graphql.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// As JpaAuditing works based on Proxy we shouldn't create configuration as proxyBeans as false
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditConfig {}
