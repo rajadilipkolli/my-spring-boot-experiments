830fb2944cfe5e52b39a7b8991faa94ffeb3a2c1
Raja Kolli <rajadilipkolli@gmail.com>
2023-08-17 14:35:04 +0000

    polish: removes wildcard Import

--- graphql/boot-graphql-webmvc/src/main/java/com/example/graphql/web/controllers/PostDetailsController.java
+++ graphql/boot-graphql-webmvc/src/main/java/com/example/graphql/web/controllers/PostDetailsController.java
@@ -7,7 +7,12 @@ import com.example.graphql.services.PostDetailsService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/postdetails")
