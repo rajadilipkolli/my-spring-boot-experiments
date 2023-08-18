49585919fbd61afb0010203d2a15108e0c295d8f
Raja Kolli <rajadilipkolli@gmail.com>
2023-08-18 12:22:28 +0530

    polish: add schema validation test

---
+++ batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/repository/SchemaValidationTest.java
@@ -0,0 +1,26 @@
+package com.example.bootbatchjpa.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.bootbatchjpa.common.TestContainersConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+
+@DataJpaTest(properties = {"spring.jpa.hibernate.ddl-auto=validate"})
+@ImportTestcontainers(TestContainersConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class SchemaValidationTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void contextLoads() {
+        assertThat(dataSource).isInstanceOf(HikariDataSource.class);
+    }
+}
