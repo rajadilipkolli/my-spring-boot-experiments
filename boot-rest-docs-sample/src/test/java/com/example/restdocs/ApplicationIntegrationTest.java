3a9608f96042ef2fd01f7f12756486db480c38c5
Raja Kolli <rajadilipkolli@gmail.com>
2023-08-20 05:12:56 +0000

    fix: sonar issues

--- boot-rest-docs-sample/src/test/java/com/example/restdocs/ApplicationIntegrationTest.java
+++ boot-rest-docs-sample/src/test/java/com/example/restdocs/ApplicationIntegrationTest.java
@@ -1,12 +1,22 @@
 package com.example.restdocs;
 
+import static org.junit.Assert.assertThat;
+
 import com.example.restdocs.common.AbstractIntegrationTest;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.TestPropertySource;
 
 @TestPropertySource(properties = {"spring.jpa.hibernate.ddl-auto=validate"})
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        assertThat(dataSource).isInstanceOf(HikariDataSource.class);
+    }
 }
