459d1060fda8d9fa5826e95ee8e0dbf5a30a0cda
Raja Kolli <rajadilipkolli@gmail.com>
2023-09-21 10:36:48 +0000

    feat: initial commit of opensearch

---
+++ boot-opensearch-sample/src/test/java/com/example/opensearch/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.example.opensearch;
+
+import com.example.opensearch.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
