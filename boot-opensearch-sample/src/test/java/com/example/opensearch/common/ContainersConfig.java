ae142c9c403c26e1761aa37541e8d1bbc3b79578
Raja Kolli <rajadilipkolli@gmail.com>
2023-09-21 12:44:01 +0000

    chore(deps): update opensearchproject/opensearch docker tag to v1.3.12

--- boot-opensearch-sample/src/test/java/com/example/opensearch/common/ContainersConfig.java
+++ boot-opensearch-sample/src/test/java/com/example/opensearch/common/ContainersConfig.java
@@ -1,32 +1,19 @@
 package com.example.opensearch.common;
 
-import java.net.HttpURLConnection;
-import java.time.Duration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainersConfig {
 
     static final GenericContainer<?> openSearchContainer =
-            new GenericContainer<>("opensearchproject/opensearch:1.1.0")
+            new GenericContainer<>("opensearchproject/opensearch:1.3.12")
                     .withEnv("discovery.type", "single-node")
                     .withEnv("DISABLE_SECURITY_PLUGIN", "true")
                     .withEnv("OPENSEARCH_JAVA_OPTS", "-Xms512m -Xmx512m")
-                    .withExposedPorts(9200, 9600)
-                    .waitingFor(
-                            new HttpWaitStrategy()
-                                    .forPort(9200)
-                                    .forStatusCodeMatching(
-                                            response ->
-                                                    response == HttpURLConnection.HTTP_OK
-                                                            || response
-                                                                    == HttpURLConnection
-                                                                            .HTTP_UNAUTHORIZED)
-                                    .withStartupTimeout(Duration.ofMinutes(4)));
+                    .withExposedPorts(9200, 9600);
 
     static {
         openSearchContainer.start();
