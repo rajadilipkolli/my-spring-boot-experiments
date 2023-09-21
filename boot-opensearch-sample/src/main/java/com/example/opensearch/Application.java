f9d043e67278b5f7f1035cd94e71141225e1c570
Raja Kolli <rajadilipkolli@gmail.com>
2023-09-21 13:44:20 +0000

    feat: fixes issue with starting app

--- boot-opensearch-sample/src/main/java/com/example/opensearch/Application.java
+++ boot-opensearch-sample/src/main/java/com/example/opensearch/Application.java
@@ -3,9 +3,10 @@ package com.example.opensearch;
 import com.example.opensearch.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
 @EnableConfigurationProperties({ApplicationProperties.class})
 public class Application {
 
