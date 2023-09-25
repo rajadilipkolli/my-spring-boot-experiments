ed63cac96f98f451ef1c8e8a89a84c23993bdc82
Raja Kolli <rajadilipkolli@gmail.com>
2023-09-25 14:44:26 +0000

    feat: fixes sonar Security Hotspot issue

--- open-api-spring-boot/src/main/java/org/example/openapi/controller/HomeController.java
+++ open-api-spring-boot/src/main/java/org/example/openapi/controller/HomeController.java
@@ -2,6 +2,7 @@ package org.example.openapi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Home redirection to OpenAPI api documentation
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "redirect:swagger-ui.html";
     }
