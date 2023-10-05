3d43638af58c3b9072e378024f30cf20379398c0
Raja Kolli <rajadilipkolli@gmail.com>
2023-10-05 11:25:17 +0530

    feat: adds JooqExceptionTranslator

--- r2dbc/boot-jooq-r2dbc-sample/src/main/java/com/example/jooq/r2dbc/config/JooqConfiguration.java
+++ r2dbc/boot-jooq-r2dbc-sample/src/main/java/com/example/jooq/r2dbc/config/JooqConfiguration.java
@@ -3,8 +3,11 @@ package com.example.jooq.r2dbc.config;
 import io.r2dbc.spi.ConnectionFactory;
 import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
+import org.jooq.impl.DefaultExecuteListenerProvider;
+import org.springframework.boot.autoconfigure.jooq.JooqExceptionTranslator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.r2dbc.connection.TransactionAwareConnectionFactoryProxy;
 
@@ -15,5 +18,11 @@ public class JooqConfiguration {
     @Bean
     public DSLContext dslContext(ConnectionFactory connectionFactory) {
         return DSL.using(new TransactionAwareConnectionFactoryProxy(connectionFactory)).dsl();
+    }
+
+    @Bean
+    @Order(0)
+    public DefaultExecuteListenerProvider jooqExceptionTranslatorExecuteListenerProvider() {
+        return new DefaultExecuteListenerProvider(new JooqExceptionTranslator());
     }
 }
