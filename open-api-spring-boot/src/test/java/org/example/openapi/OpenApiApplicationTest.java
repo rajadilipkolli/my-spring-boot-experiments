b3a916d0a73653fa8d13766c95be0662b9a83483
rajadilipkolli <rajadilipkolli@gmail.com>
2023-10-19 11:08:28 +0530

    feat: using generated code

--- open-api-spring-boot/src/test/java/org/example/openapi/OpenApiApplicationTest.java
+++ open-api-spring-boot/src/test/java/org/example/openapi/OpenApiApplicationTest.java
@@ -3,13 +3,16 @@ package org.example.openapi;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.openapitools.OpenApiGeneratorApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.junit.jupiter.api.Test;
 
-@SpringBootTest
+@SpringBootTest(classes = OpenApiGeneratorApplication.class)
 @AutoConfigureMockMvc
 class OpenApiApplicationTest {
 
@@ -29,7 +32,7 @@ class OpenApiApplicationTest {
 
     @Test
     void whenReadCustomerById_thenStatusIsNotImplemented() throws Exception {
-        this.mockMvc.perform(get("/api/customers/1"))
+        this.mockMvc.perform(get("/api/customers/1").header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE))
             .andExpect(status().isNotImplemented());
     }
 
