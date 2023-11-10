f0b3882fefe488843c9f557e079338fe3877c7d3
Raja Kolli <rajadilipkolli@gmail.com>
2023-11-10 18:17:52 +0000

    chore: adds datasource proxy for printing queries

---
+++ graphql/boot-graphql-querydsl/src/main/java/com/example/graphql/querydsl/config/DatasourceProxyBeanPostProcessor.java
@@ -0,0 +1,53 @@
+package com.example.graphql.querydsl.config;
+
+import java.lang.reflect.Method;
+import javax.sql.DataSource;
+import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
+import net.ttddyy.dsproxy.support.ProxyDataSource;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+@Component
+public class DatasourceProxyBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+        if (bean instanceof DataSource dataSource && !(bean instanceof ProxyDataSource)) {
+            final ProxyFactory factory = new ProxyFactory(bean);
+            factory.setProxyTargetClass(true);
+            factory.addAdvice(new ProxyDataSourceInterceptor(dataSource));
+            return factory.getProxy();
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+        return bean;
+    }
+
+    private record ProxyDataSourceInterceptor(DataSource dataSource) implements MethodInterceptor {
+        private ProxyDataSourceInterceptor(final DataSource dataSource) {
+            this.dataSource = ProxyDataSourceBuilder.create(dataSource)
+                    .name("MyDS")
+                    .multiline()
+                    .logQueryBySlf4j(SLF4JLogLevel.INFO)
+                    .build();
+        }
+
+        @Override
+        public Object invoke(final MethodInvocation invocation) throws Throwable {
+            final Method proxyMethod = ReflectionUtils.findMethod(
+                    this.dataSource.getClass(), invocation.getMethod().getName());
+            if (proxyMethod != null) {
+                return proxyMethod.invoke(this.dataSource, invocation.getArguments());
+            }
+            return invocation.proceed();
+        }
+    }
+}
