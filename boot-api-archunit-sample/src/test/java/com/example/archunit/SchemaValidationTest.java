2d63f4c7c6bbc6ed257b9f7b0f7f116ae8bb2244
Raja Kolli <rajadilipkolli@gmail.com>
2023-11-16 01:15:51 +0000

    #876 - Upgrade to Java 21 and SB 3.2.0-RC2

--- boot-api-archunit-sample/src/test/java/com/example/archunit/SchemaValidationTest.java
+++ boot-api-archunit-sample/src/test/java/com/example/archunit/SchemaValidationTest.java
@@ -1,16 +1,24 @@
 package com.example.archunit;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest(
-        properties = {
-            "spring.jpa.hibernate.ddl-auto=validate",
-            "spring.test.database.replace=none",
-            "spring.datasource.url=jdbc:tc:postgresql:16.0-alpine:///db"
-        })
+import com.example.archunit.common.ContainersConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@DataJpaTest(properties = {"spring.jpa.hibernate.ddl-auto=validate", "spring.test.database.replace=none"})
+@Import(ContainersConfig.class)
 class SchemaValidationTest {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    void validateJpaMappingsWithDbSchema() {}
+    void validateJpaMappingsWithDbSchema() {
+        assertThat(dataSource).isInstanceOf(HikariDataSource.class);
+    }
 }
