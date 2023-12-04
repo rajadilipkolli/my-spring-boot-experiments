47f795d876fd8fb4c68f3f90df4592d59dc4e589
Raja Kolli <rajadilipkolli@gmail.com>
2023-12-04 15:46:42 +0000

    feat : fix running the tests

--- jpa/multitenancy/multidatasource-multitenancy/src/test/java/com/example/multitenancy/common/DBContainerInitializer.java
+++ jpa/multitenancy/multidatasource-multitenancy/src/test/java/com/example/multitenancy/common/DBContainerInitializer.java
@@ -3,24 +3,25 @@ package com.example.multitenancy.common;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.oracle.OracleContainer;
 import org.testcontainers.utility.DockerImageName;
 
-@Testcontainers(disabledWithoutDocker = true, parallel = true)
 public class DBContainerInitializer {
 
-    @Container
     private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER =
-            new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("16.1-alpine"));
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("16.1-alpine"))
+                    .withReuse(true);
 
-    @Container
     private static final OracleContainer ORACLE_CONTAINER =
             new OracleContainer(
                             DockerImageName.parse("gvenzl/oracle-free")
                                     .withTag("23-slim-faststart"))
                     .withReuse(true);
+
+    static {
+        Startables.deepStart(POSTGRE_SQL_CONTAINER, ORACLE_CONTAINER).join();
+    }
 
     @DynamicPropertySource
     static void addsDynamicProperties(DynamicPropertyRegistry propertyRegistry) {
