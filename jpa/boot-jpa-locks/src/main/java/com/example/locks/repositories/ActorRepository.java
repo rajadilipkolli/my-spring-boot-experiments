09273bf1e7ef7b238739f2e2a18ebdd1a89e6132
ChennuruAkhil <44938089+ChennuruAkhil@users.noreply.github.com>
2024-02-16 22:26:55 +0530

    Added pessimistic write lock test scenario (#1092)
    * ChennuruAkhil/my-spring-boot-experiments#8 Added pessimistic write lock test scenario
    
    * Update pom.xml
    
    * revert back
    
    * ChennuruAkhil/my-spring-boot-experiments#8 Modified the test case to use test profile
    
    * ChennuruAkhil/my-spring-boot-experiments#8 Fixed test case
    
    * Pessimistic Locking Integration ChennuruAkhil/my-spring-boot-experiments#8 Spotless formatting
    
    * Pessimistic Locking Integration ChennuruAkhil/my-spring-boot-experiments#8 Fixed build issue
    
    ---------
    
    Co-authored-by: Raja Kolli <rajadilipkolli@gmail.com>

--- jpa/boot-jpa-locks/src/main/java/com/example/locks/repositories/ActorRepository.java
+++ jpa/boot-jpa-locks/src/main/java/com/example/locks/repositories/ActorRepository.java
@@ -3,4 +3,4 @@ package com.example.locks.repositories;
 import com.example.locks.entities.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActorRepository extends JpaRepository<Actor, Long> {}
+public interface ActorRepository extends JpaRepository<Actor, Long>, CustomizedActorRepository {}
