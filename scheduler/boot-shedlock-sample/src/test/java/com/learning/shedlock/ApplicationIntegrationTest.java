999a9d6583b6fcf1c97a29347b1d2c28436b09e4
Raja Kolli <rajadilipkolli@gmail.com>
2024-02-19 15:34:48 +0000

    feat : initial commit of shedlock sample

---
+++ scheduler/boot-shedlock-sample/src/test/java/com/learning/shedlock/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.learning.shedlock;
+
+import com.learning.shedlock.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
