977b83cec1a573837d731d6ff63916f2d1aef72a
Raja Kolli <rajadilipkolli@gmail.com>
2024-02-19 15:51:58 +0000

    feat : adds lock config

---
+++ scheduler/boot-shedlock-sample/src/main/java/com/learning/shedlock/config/LockConfig.java
@@ -0,0 +1,24 @@
+package com.learning.shedlock.config;
+
+import javax.sql.DataSource;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT2M")
+public class LockConfig {
+
+    @Bean
+    LockProvider scheduledJobLockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(JdbcTemplateLockProvider.Configuration.builder()
+                .withJdbcTemplate(new JdbcTemplate(dataSource))
+                .usingDbTime()
+                .build());
+    }
+}
