d0763adb80c8f3db07a1bc54087b8d406f737266
Raja Kolli <rajadilipkolli@gmail.com>
2024-02-19 16:04:06 +0000

    feat : adds sample job scheduler

--- scheduler/boot-shedlock-sample/src/main/java/com/learning/shedlock/config/Initializer.java
+++ scheduler/boot-shedlock-sample/src/main/java/com/learning/shedlock/config/Initializer.java
@@ -1,19 +1,18 @@
 package com.learning.shedlock.config;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
-public class Initializer implements CommandLineRunner {
+public class Initializer {
 
-    private final ApplicationProperties properties;
-
-    @Override
-    public void run(String... args) {
-        log.info("Running Initializer.....");
+    @Scheduled(cron = "0 */1 * * * *")
+    @SchedulerLock(name = "lockedTask", lockAtMostFor = "PT5S", lockAtLeastFor = "PT2S")
+    public void lockedTask() {
+        // code for task to be performed
+        log.info("Scheduler Job triggered");
     }
 }
--- scheduler/boot-shedlock-sample/src/main/resources/db/changelog/migration/01-init.sql
+++ scheduler/boot-shedlock-sample/src/main/resources/db/changelog/migration/01-init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE lockProvider (
+CREATE TABLE shedlock (
  name VARCHAR(64),
  lock_until TIMESTAMP(3) NULL,
  locked_at TIMESTAMP(3) NULL,
