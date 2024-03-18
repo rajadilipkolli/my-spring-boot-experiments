dddf633aba507b614299d09064f384cacdc287c1
ChennuruAkhil <44938089+ChennuruAkhil@users.noreply.github.com>
2024-03-18 20:24:36 +0530

    ChennuruAkhil/my-spring-boot-experiments#8 Refactored code with Query… (#1148)
    * ChennuruAkhil/my-spring-boot-experiments#8 Refactored code with Query Hints
    
    * ChennuruAkhil/my-spring-boot-experiments#8 Fixed Sonar issues
    
    * ChennuruAkhil/my-spring-boot-experiments#8 Refactored code as per the comments

--- jpa/boot-jpa-locks/src/main/java/com/example/locks/repositories/ActorRepository.java
+++ jpa/boot-jpa-locks/src/main/java/com/example/locks/repositories/ActorRepository.java
@@ -1,6 +1,29 @@
 package com.example.locks.repositories;
 
 import com.example.locks.entities.Actor;
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
-public interface ActorRepository extends JpaRepository<Actor, Long>, CustomizedActorRepository {}
+public interface ActorRepository extends JpaRepository<Actor, Long> {
+
+    @Query(value = "SELECT actor FROM Actor actor WHERE actor.actorId = :actorId")
+    @QueryHints(
+            value = {
+                @QueryHint(name = "jakarta.persistence.lock.timeout", value = "5000"),
+                @QueryHint(name = "jakarta.persistence.query.timeout", value = "3000"),
+            })
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Actor> findByIdWithPessimisticWriteLock(@Param("actorId") Long actorId);
+
+    @Query(value = "SELECT actor FROM Actor actor WHERE actor.actorId = :actorId")
+    @QueryHints(
+            value = {
+                @QueryHint(name = "jakarta.persistence.lock.timeout", value = "5000"),
+                @QueryHint(name = "jakarta.persistence.query.timeout", value = "3000"),
+            })
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    Optional<Actor> findByIdWithPessimisticReadLock(@Param("actorId") Long actorId);
+}
