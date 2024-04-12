558c2304a82e396f8195b440a9a75fac0ed8368b
Raja Kolli <rajadilipkolli@gmail.com>
2024-04-12 11:49:34 +0000

    feat : fetch grafana port

--- boot-grafana-lgtm/src/test/java/com/learning/grafanalgtm/GrafanaLGTMApplicationTest.java
+++ boot-grafana-lgtm/src/test/java/com/learning/grafanalgtm/GrafanaLGTMApplicationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.GenericContainer;
 
 @SpringBootTest(classes = {TestGrafanaLGTMApplication.class})
 @AutoConfigureMockMvc
@@ -13,8 +14,11 @@ class GrafanaLGTMApplicationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private GenericContainer<?> lgtmContainer;
+
     @Test
     void prometheus(){
-
+        System.out.println("Configured grafana port "+ lgtmContainer.getMappedPort(3000));
     }
 }
