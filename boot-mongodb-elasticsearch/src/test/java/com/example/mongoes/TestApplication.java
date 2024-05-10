372058afca69d079f7ffb2a4818755ead1c2350e
Raja Kolli <rajadilipkolli@gmail.com>
2024-05-10 18:01:31 +0530

    chore(deps): update mongo docker tag to v7.0.9

--- boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/TestApplication.java
+++ boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/TestApplication.java
@@ -25,7 +25,7 @@ public class TestApplication {
     @Bean
     @RestartScope
     MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("7.0.8")).withSharding();
+        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("7.0.9")).withSharding();
     }
 
     public static void main(String[] args) {
