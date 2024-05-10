555e5edecac7af4a648d6ffb0e62744b9e6d394a
Raja Kolli <rajadilipkolli@gmail.com>
2024-05-10 13:29:56 +0000

    fix : sync with other declarations

--- batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/common/TestContainersConfig.java
+++ batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/common/TestContainersConfig.java
@@ -2,9 +2,11 @@ package com.example.bootbatchjpa.common;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 public interface TestContainersConfig {
 
     @ServiceConnection
-    PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.3-alpine");
+    PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.3-alpine"));
 }
