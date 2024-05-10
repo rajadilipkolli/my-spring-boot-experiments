f0ea3f714ae1b9e08e96e35b7642e840f5030e42
Raja Kolli <rajadilipkolli@gmail.com>
2024-05-10 13:24:35 +0000

    feat : use testcontainers in dev mode

---
+++ jpa/multitenancy/partition/src/test/java/com/example/multitenancy/partition/TestApplication.java
@@ -0,0 +1,22 @@
+package com.example.multitenancy.partition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.3-alpine"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestApplication.class).run(args);
+    }
+}
