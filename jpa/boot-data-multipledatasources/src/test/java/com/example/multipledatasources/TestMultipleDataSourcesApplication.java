7c66ffb36ce83d91fb8c79579a4cb3bda8066c8d
Raja Kolli <rajadilipkolli@gmail.com>
2024-07-01 21:51:12 +0530

    address review comments

--- jpa/boot-data-multipledatasources/src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
+++ jpa/boot-data-multipledatasources/src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
@@ -7,25 +7,33 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
-class TestMultipleDataSourcesApplication {
+@TestConfiguration
+public class TestMultipleDataSourcesApplication {
 
-    private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>("mysql:8.4");
+    private static final MySQLContainer<?> MY_SQL_CONTAINER =
+            new MySQLContainer<>(DockerImageName.parse("mysql").withTag("8.4"));
 
     private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER =
-            new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.3-alpine"));
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("16.3-alpine"));
 
     static {
         Startables.deepStart(MY_SQL_CONTAINER, POSTGRE_SQL_CONTAINER).join();
+        Runtime.getRuntime()
+                .addShutdownHook(
+                        new Thread(
+                                () -> {
+                                    MY_SQL_CONTAINER.stop();
+                                    POSTGRE_SQL_CONTAINER.stop();
+                                }));
+    }
+
+    public static void main(String[] args) {
         System.setProperty("app.datasource.cardholder.url", MY_SQL_CONTAINER.getJdbcUrl());
         System.setProperty("app.datasource.cardholder.username", MY_SQL_CONTAINER.getUsername());
         System.setProperty("app.datasource.cardholder.password", MY_SQL_CONTAINER.getPassword());
         System.setProperty("app.datasource.member.url", POSTGRE_SQL_CONTAINER.getJdbcUrl());
         System.setProperty("app.datasource.member.username", POSTGRE_SQL_CONTAINER.getUsername());
         System.setProperty("app.datasource.member.password", POSTGRE_SQL_CONTAINER.getPassword());
-    }
-
-    public static void main(String[] args) {
         SpringApplication.from(MultipleDataSourcesApplication::main)
                 .with(TestMultipleDataSourcesApplication.class)
                 .run(args);
