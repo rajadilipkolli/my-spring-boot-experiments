32c6f91d3d00d08d61818ab0b205d5ac5ac4a7e3
Raja Kolli <rajadilipkolli@gmail.com>
2024-08-21 18:39:37 +0000

    initial commit of reactive

--- r2dbc/boot-reactive-cache/src/main/java/com/example/cache/entities/Movie.java
+++ r2dbc/boot-reactive-cache/src/main/java/com/example/cache/entities/Movie.java
@@ -1,24 +1,10 @@
 package com.example.cache.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import org.springframework.data.relational.core.mapping.Table;
 import java.util.Objects;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.Hibernate;
 
 @Entity
 @Table(name = "movies")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Movie {
 
     @Id
@@ -28,16 +14,4 @@ public class Movie {
     @Column(nullable = false)
     private String text;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Movie movie = (Movie) o;
-        return id != null && Objects.equals(id, movie.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
