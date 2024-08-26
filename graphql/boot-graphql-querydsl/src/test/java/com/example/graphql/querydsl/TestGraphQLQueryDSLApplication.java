5d3b9b26dc406ec1b005a6b0c61bbc721a988ebd
Raja Kolli <rajadilipkolli@gmail.com>
2024-08-26 17:20:42 +0000

    feat : use record for Embeddable

--- graphql/boot-graphql-querydsl/src/test/java/com/example/graphql/querydsl/TestGraphQLQueryDSLApplication.java
+++ graphql/boot-graphql-querydsl/src/test/java/com/example/graphql/querydsl/TestGraphQLQueryDSLApplication.java
@@ -3,10 +3,12 @@ package com.example.graphql.querydsl;
 import com.example.graphql.querydsl.common.ContainersConfig;
 import org.springframework.boot.SpringApplication;
 
-public class TestApplication {
+public class TestGraphQLQueryDSLApplication {
 
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "local");
-        SpringApplication.from(Application::main).with(ContainersConfig.class).run(args);
+        SpringApplication.from(GraphQLQueryDSLApplication::main)
+                .with(ContainersConfig.class)
+                .run(args);
     }
 }
