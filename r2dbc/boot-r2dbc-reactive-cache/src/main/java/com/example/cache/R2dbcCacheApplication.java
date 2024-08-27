c25213df8d9689ba68a2f1338ae68143f6562e1e
Raja Kolli <rajadilipkolli@gmail.com>
2024-08-27 13:34:04 +0000

    rename

--- r2dbc/boot-r2dbc-reactive-cache/src/main/java/com/example/cache/R2dbcCacheApplication.java
+++ r2dbc/boot-r2dbc-reactive-cache/src/main/java/com/example/cache/R2dbcCacheApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties({ApplicationProperties.class})
-public class Application {
+public class R2dbcCacheApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(R2dbcCacheApplication.class, args);
     }
 }
