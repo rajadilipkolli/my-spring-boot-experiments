9d71560161513a99aa7d09fa5958eb335450190e
Raja Kolli <rajadilipkolli@gmail.com>
2024-08-31 07:36:24 +0000

    fix : build issues

--- boot-ultimate-redis/src/test/java/com/example/ultimateredis/common/AbstractIntegrationTest.java
+++ boot-ultimate-redis/src/test/java/com/example/ultimateredis/common/AbstractIntegrationTest.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ActiveProfiles({AppConstants.PROFILE_NOT_CLUSTER})
+@ActiveProfiles({AppConstants.PROFILE_STANDALONE})
 @SpringBootTest(
         webEnvironment = RANDOM_PORT,
         classes = {TestcontainersConfiguration.class})
