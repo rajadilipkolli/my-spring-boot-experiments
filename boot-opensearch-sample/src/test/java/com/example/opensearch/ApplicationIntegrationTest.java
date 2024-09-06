65e1987592ba719bbb80075ecd34d308630428a2
Raja Kolli <rajadilipkolli@gmail.com>
2024-09-06 12:37:06 +0000

    feat  : adds health checks and testcase

--- boot-opensearch-sample/src/test/java/com/example/opensearch/ApplicationIntegrationTest.java
+++ boot-opensearch-sample/src/test/java/com/example/opensearch/ApplicationIntegrationTest.java
@@ -1,10 +1,29 @@
 package com.example.opensearch;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.example.opensearch.common.AbstractIntegrationTest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
 import org.junit.jupiter.api.Test;
+import org.opensearch.client.Request;
+import org.opensearch.client.Response;
+import org.opensearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
+    @Autowired private RestClient restClient;
+
     @Test
-    void contextLoads() {}
+    void restClientOpenSearchNodeVersion() throws IOException {
+        final Request request = new Request("GET", "/");
+        final Response response = restClient.performRequest(request);
+        try (InputStream input = response.getEntity().getContent()) {
+            JsonNode result = new ObjectMapper().readTree(input);
+            assertThat(result.path("version").path("number").asText()).isEqualTo("2.16.0");
+        }
+    }
 }
