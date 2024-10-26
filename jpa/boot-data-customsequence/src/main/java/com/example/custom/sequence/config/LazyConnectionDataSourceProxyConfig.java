6f6fdeff19b6ab382e0daa700f3ec7cd7f674f89
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-26 18:28:44 +0000

    correct way of using

--- jpa/boot-data-customsequence/src/main/java/com/example/custom/sequence/config/LazyConnectionDataSourceProxyConfig.java
+++ jpa/boot-data-customsequence/src/main/java/com/example/custom/sequence/config/LazyConnectionDataSourceProxyConfig.java
@@ -1,24 +1,17 @@
 package com.example.custom.sequence.config;
 
-import com.zaxxer.hikari.HikariDataSource;
 import io.hypersistence.utils.logging.InlineQueryLogEntryCreator;
 import javax.sql.DataSource;
 import net.ttddyy.dsproxy.listener.ChainListener;
 import net.ttddyy.dsproxy.listener.DataSourceQueryCountListener;
 import net.ttddyy.dsproxy.listener.logging.SLF4JQueryLoggingListener;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
-import org.springframework.util.StringUtils;
 
 /**
  * Configuration class that sets up a lazy-loading data source proxy to improve performance by
@@ -26,52 +19,16 @@ import org.springframework.util.StringUtils;
  * particularly useful in scenarios where database operations don't always occur during request
  * processing.
  */
-@Configuration(proxyBeanMethods = false)
+@AutoConfiguration(
+        after = {DataSourceAutoConfiguration.class},
+        before = {HibernateJpaAutoConfiguration.class})
 class LazyConnectionDataSourceProxyConfig {
 
     private static final String DATA_SOURCE_PROXY_NAME = "customSeqDsProxy";
-    private static final Logger log =
-            LoggerFactory.getLogger(LazyConnectionDataSourceProxyConfig.class);
-
-    private final DataSourceProperties dataSourceProperties;
-
-    LazyConnectionDataSourceProxyConfig(DataSourceProperties dataSourceProperties) {
-        this.dataSourceProperties = dataSourceProperties;
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")
-    HikariDataSource hikariDataSource(ConfigurableListableBeanFactory beanFactory) {
-        JdbcConnectionDetails connectionDetails;
-        try {
-            // Try to get JdbcConnectionDetails bean if available
-            connectionDetails = beanFactory.getBean(JdbcConnectionDetails.class);
-            dataSourceProperties.setDriverClassName(connectionDetails.getDriverClassName());
-            dataSourceProperties.setUrl(connectionDetails.getJdbcUrl());
-            dataSourceProperties.setUsername(connectionDetails.getUsername());
-            dataSourceProperties.setPassword(connectionDetails.getPassword());
-        } catch (NoSuchBeanDefinitionException e) {
-            // Ignore as JdbcConnectionDetails might not be available in non-test environments
-            log.debug("JdbcConnectionDetails bean not found, falling back to properties", e);
-        }
-
-        // Create and configure HikariDataSource with properties
-        HikariDataSource hikariDataSource =
-                dataSourceProperties
-                        .initializeDataSourceBuilder()
-                        .type(HikariDataSource.class)
-                        .build();
-
-        if (StringUtils.hasText(dataSourceProperties.getName())) {
-            hikariDataSource.setPoolName(dataSourceProperties.getName());
-        }
-
-        return hikariDataSource;
-    }
 
     @Bean
     @Primary
-    DataSource dataSource(HikariDataSource hikariDataSource) {
+    DataSource lazyConnectionDataSourceProxy(DataSource dataSource) {
         // Add listeners for query logging
         ChainListener listener = new ChainListener();
         SLF4JQueryLoggingListener loggingListener = new SLF4JQueryLoggingListener();
@@ -81,7 +38,7 @@ class LazyConnectionDataSourceProxyConfig {
 
         // Wrap with ProxyDataSource and LazyConnectionDataSourceProxy
         DataSource loggingDataSource =
-                ProxyDataSourceBuilder.create(hikariDataSource)
+                ProxyDataSourceBuilder.create(dataSource)
                         .name(DATA_SOURCE_PROXY_NAME)
                         .listener(listener)
                         .build();
