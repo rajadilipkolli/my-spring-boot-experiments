a0777c33a3ff88897831098504034429c5abb9fb
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-03 16:02:17 +0000

    feat: create saperate testcontainers config

---
+++ boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/common/ContainersConfig.java
@@ -0,0 +1,29 @@
+package com.example.mongoes.common;
+
+import java.util.Map;
+import org.springframework.boot.devtools.restart.RestartScope;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class ContainersConfig {
+
+    @ServiceConnection
+    @Bean
+    @RestartScope
+    ElasticsearchContainer elasticsearchContainer() {
+        return new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:8.16.1")
+                .withEnv(Map.of("xpack.security.enabled", "false"));
+    }
+
+    @ServiceConnection
+    @Bean
+    @RestartScope
+    MongoDBContainer mongoDBContainer() {
+        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.0.3")).withSharding();
+    }
+}
