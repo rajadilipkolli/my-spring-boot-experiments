703a0d317afc3b1c4ea27d429042bfaa58e14b1f
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-03 21:57:39 +0530

    feat : polish r2dbc and upgrade postgres (#1550)

--- r2dbc/boot-jooq-r2dbc-sample/src/test/java/com/example/jooq/r2dbc/common/AbstractIntegrationTest.java
+++ r2dbc/boot-jooq-r2dbc-sample/src/test/java/com/example/jooq/r2dbc/common/AbstractIntegrationTest.java
@@ -3,7 +3,6 @@ package com.example.jooq.r2dbc.common;
 import static com.example.jooq.r2dbc.utils.AppConstants.PROFILE_TEST;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import com.example.jooq.r2dbc.TestApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -12,7 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @ActiveProfiles({PROFILE_TEST})
-@SpringBootTest(webEnvironment = RANDOM_PORT, classes = TestApplication.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = ContainerConfig.class)
 @AutoConfigureWebTestClient
 public abstract class AbstractIntegrationTest {
 
--- r2dbc/boot-r2dbc-sample/src/test/java/com/example/bootr2dbc/common/AbstractIntegrationTest.java
+++ r2dbc/boot-r2dbc-sample/src/test/java/com/example/bootr2dbc/common/AbstractIntegrationTest.java
@@ -3,7 +3,6 @@ package com.example.bootr2dbc.common;
 import static com.example.bootr2dbc.utils.AppConstants.PROFILE_TEST;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import com.example.bootr2dbc.TestApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +12,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @ActiveProfiles({PROFILE_TEST})
 @SpringBootTest(
         webEnvironment = RANDOM_PORT,
-        classes = {TestApplication.class})
+        classes = {ContainerConfig.class})
 @AutoConfigureWebTestClient
 public abstract class AbstractIntegrationTest {
 
