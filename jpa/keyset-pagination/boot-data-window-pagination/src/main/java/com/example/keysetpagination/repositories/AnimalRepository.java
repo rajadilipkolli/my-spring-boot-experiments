60f0a6229a8391e89010c10878520e72a72b46ab
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-05 12:49:44 +0530

    feat : enhance tests (#1555)

--- jpa/keyset-pagination/boot-data-window-pagination/src/main/java/com/example/keysetpagination/repositories/AnimalRepository.java
+++ jpa/keyset-pagination/boot-data-window-pagination/src/main/java/com/example/keysetpagination/repositories/AnimalRepository.java
@@ -2,5 +2,7 @@ package com.example.keysetpagination.repositories;
 
 import com.example.keysetpagination.entities.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AnimalRepository extends JpaRepository<Animal, Long>, CustomRepository<Animal> {}
+public interface AnimalRepository
+        extends JpaRepository<Animal, Long>, CustomRepository<Animal>, JpaSpecificationExecutor<Animal> {}
