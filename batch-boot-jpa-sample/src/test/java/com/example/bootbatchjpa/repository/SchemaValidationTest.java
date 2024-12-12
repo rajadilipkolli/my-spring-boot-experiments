d9a1cd9a6e53ac7614fbf4be19c259d5bf547b8a
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-12 17:09:52 +0530

    upgrade to springboot 3.4 (#1535)
    * upgrade to springboot 3.4
    
    * attempt to fix
    
    * fix and polish
    
    * fix : junit failures

--- batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/repository/SchemaValidationTest.java
+++ batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/repository/SchemaValidationTest.java
@@ -2,20 +2,18 @@ package com.example.bootbatchjpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.example.bootbatchjpa.common.TestContainersConfig;
+import com.example.bootbatchjpa.common.ContainersConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.context.annotation.Import;
 
-@DataJpaTest(
-        properties = {
-            "spring.jpa.hibernate.ddl-auto=validate",
-            "spring.test.database.replace=none"
-        })
-@ImportTestcontainers(TestContainersConfig.class)
+@DataJpaTest(properties = {"spring.jpa.hibernate.ddl-auto=validate"})
+@Import(ContainersConfig.class)
+@AutoConfigureTestDatabase
 class SchemaValidationTest {
 
     @Autowired private DataSource dataSource;
