d808bebfdaef2ed9699b334236e948bf9f365d5c
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-25 18:17:13 +0000

    polish db operations

--- jpa/boot-read-replica-postgresql/src/main/java/com/example/demo/readreplica/repository/ArticleRepository.java
+++ jpa/boot-read-replica-postgresql/src/main/java/com/example/demo/readreplica/repository/ArticleRepository.java
@@ -5,11 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
-    @Transactional(readOnly = true)
     @Query("select a from Article a left join fetch a.comments where a.id = :articleId  ")
     Optional<Article> findByArticleId(@Param("articleId") Long articleId);
 }
