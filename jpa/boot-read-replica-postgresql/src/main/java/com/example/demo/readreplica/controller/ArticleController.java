8beedfa3ca024173e6af86ee4b71349f0070a306
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-25 17:43:56 +0000

    fix wildcard import

--- jpa/boot-read-replica-postgresql/src/main/java/com/example/demo/readreplica/controller/ArticleController.java
+++ jpa/boot-read-replica-postgresql/src/main/java/com/example/demo/readreplica/controller/ArticleController.java
@@ -4,7 +4,13 @@ import com.example.demo.readreplica.domain.ArticleDTO;
 import com.example.demo.readreplica.service.ArticleService;
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
--- jpa/boot-read-replica-postgresql/src/test/java/com/example/demo/readreplica/controller/ArticleControllerIntTest.java
+++ jpa/boot-read-replica-postgresql/src/test/java/com/example/demo/readreplica/controller/ArticleControllerIntTest.java
@@ -55,7 +55,7 @@ class ArticleControllerIntTest {
     }
 
     @Test
-    void saveArticle() throws JsonProcessingException {
+    void saveArticleRetriveAndDelete() throws JsonProcessingException {
         ArticleDTO articleDTO =
                 new ArticleDTO(
                         "junitTitle",
