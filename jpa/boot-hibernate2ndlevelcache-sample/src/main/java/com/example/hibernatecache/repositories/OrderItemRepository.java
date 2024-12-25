f0f59ba71e6cb96a88028bc5f1295f8f0905dbe2
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-25 19:19:44 +0000

    polish : Hibernate2nd level cache

--- jpa/boot-hibernate2ndlevelcache-sample/src/main/java/com/example/hibernatecache/repositories/OrderItemRepository.java
+++ jpa/boot-hibernate2ndlevelcache-sample/src/main/java/com/example/hibernatecache/repositories/OrderItemRepository.java
@@ -2,13 +2,10 @@ package com.example.hibernatecache.repositories;
 
 import com.example.hibernatecache.entities.OrderItem;
 import io.hypersistence.utils.spring.repository.BaseJpaRepository;
-import io.hypersistence.utils.spring.repository.HibernateRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface OrderItemRepository
-        extends BaseJpaRepository<OrderItem, Long>,
-                HibernateRepository<OrderItem>,
-                PagingAndSortingRepository<OrderItem, Long> {
+        extends BaseJpaRepository<OrderItem, Long>, PagingAndSortingRepository<OrderItem, Long> {
 
     void deleteAll();
 }
--- jpa/boot-hibernate2ndlevelcache-sample/src/main/java/com/example/hibernatecache/repositories/OrderRepository.java
+++ jpa/boot-hibernate2ndlevelcache-sample/src/main/java/com/example/hibernatecache/repositories/OrderRepository.java
@@ -2,11 +2,9 @@ package com.example.hibernatecache.repositories;
 
 import com.example.hibernatecache.entities.Order;
 import io.hypersistence.utils.spring.repository.BaseJpaRepository;
-import io.hypersistence.utils.spring.repository.HibernateRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface OrderRepository
-        extends BaseJpaRepository<Order, Long>, HibernateRepository<Order>, PagingAndSortingRepository<Order, Long> {
+public interface OrderRepository extends BaseJpaRepository<Order, Long>, PagingAndSortingRepository<Order, Long> {
 
     void deleteAll();
 }
