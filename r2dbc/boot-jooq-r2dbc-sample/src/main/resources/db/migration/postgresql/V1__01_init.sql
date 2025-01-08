5d6c80d657d7393b36872cea4bec076b2e575f92
Raja Kolli <rajadilipkolli@gmail.com>
2025-01-08 17:48:41 +0000

    format sql and removes unnecessary initialization

--- r2dbc/boot-jooq-r2dbc-sample/src/main/resources/db/migration/postgresql/V1__01_init.sql
+++ r2dbc/boot-jooq-r2dbc-sample/src/main/resources/db/migration/postgresql/V1__01_init.sql
@@ -1,42 +1,49 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE
+    EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE posts
-(
-    ID uuid NOT NULL DEFAULT uuid_generate_v4 (),
-    TITLE text,
-    CONTENT text,
-    STATUS varchar(50),
-    created_at timestamptz DEFAULT NOW(),
-    created_by text,
-    updated_at timestamptz,
-    version int DEFAULT 0,
-    PRIMARY KEY   (ID)
-);
+CREATE
+    TABLE
+        posts(
+            id uuid NOT NULL DEFAULT uuid_generate_v4(),
+            title text,
+            content text,
+            status text,
+            created_at timestamptz DEFAULT NOW(),
+            created_by text,
+            updated_at timestamptz,
+            version INT DEFAULT 0,
+            PRIMARY KEY(id)
+        );
 
-create table post_comments
-(
-    id uuid not null DEFAULT uuid_generate_v4 (),
-    content text,
-    created_at timestamptz DEFAULT NOW(),
-    POST_ID uuid,
-    primary key (id),
-    CONSTRAINT FK_POST_COMMENTS FOREIGN KEY (POST_ID) REFERENCES POSTS(ID)
-);
+CREATE
+    TABLE
+        post_comments(
+            id uuid NOT NULL DEFAULT uuid_generate_v4(),
+            content text,
+            created_at timestamptz DEFAULT NOW(),
+            post_id uuid,
+            PRIMARY KEY(id),
+            CONSTRAINT FK_POST_COMMENTS FOREIGN KEY(post_id) REFERENCES POSTS(id)
+        );
 
-create table tags
-(
-    id uuid not null DEFAULT uuid_generate_v4 (),
-    name text unique,
-    created_at timestamptz DEFAULT NOW(),
-    primary key (id)
-);
+CREATE
+    TABLE
+        tags(
+            id uuid NOT NULL DEFAULT uuid_generate_v4(),
+            name text UNIQUE,
+            created_at timestamptz DEFAULT NOW(),
+            PRIMARY KEY(id)
+        );
 
-CREATE TABLE posts_tags
-(
-    post_id UUID NOT NULL,
-    tag_id UUID NOT NULL,
-    CONSTRAINT FK_POST_TAGS_PID FOREIGN KEY (post_id) REFERENCES posts(id),
-    CONSTRAINT FK_POST_TAGS_TID FOREIGN KEY (tag_id) REFERENCES tags(id),
-    CONSTRAINT UK_POST_TAGS UNIQUE (post_id, tag_id)
-);
-
+CREATE
+    TABLE
+        posts_tags(
+            post_id UUID NOT NULL,
+            tag_id UUID NOT NULL,
+            CONSTRAINT FK_POST_TAGS_PID FOREIGN KEY(post_id) REFERENCES posts(id),
+            CONSTRAINT FK_POST_TAGS_TID FOREIGN KEY(tag_id) REFERENCES tags(id),
+            CONSTRAINT UK_POST_TAGS UNIQUE(
+                post_id,
+                tag_id
+            )
+        );
