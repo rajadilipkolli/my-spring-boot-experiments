82402d5111d4e1b7910b850f83200bf17a81a3eb
Raja Kolli <rajadilipkolli@gmail.com>
2025-01-17 10:20:01 +0530

    feat : removes unnecessary `@AutoConfigureTestDatabase` (#1655)

--- boot-api-archunit-sample/src/test/java/com/example/archunit/SchemaValidationTest.java
+++ boot-api-archunit-sample/src/test/java/com/example/archunit/SchemaValidationTest.java
@@ -7,13 +7,11 @@ import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 @DataJpaTest(properties = {"spring.jpa.hibernate.ddl-auto=validate"})
 @Import(ContainersConfig.class)
-@AutoConfigureTestDatabase
 class SchemaValidationTest {
 
     @Autowired
--- graphql/boot-graphql-webmvc/src/test/java/com/example/graphql/repositories/SchemaValidationTest.java
+++ graphql/boot-graphql-webmvc/src/test/java/com/example/graphql/repositories/SchemaValidationTest.java
@@ -7,12 +7,10 @@ import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
 @DataJpaTest(properties = {"spring.jpa.hibernate.ddl-auto=validate"})
-@AutoConfigureTestDatabase
 @ImportTestcontainers(TestContainersConfig.class)
 class SchemaValidationTest {
 
