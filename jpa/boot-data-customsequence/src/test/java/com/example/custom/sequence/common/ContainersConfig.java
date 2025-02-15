4da11df65656820290249c9e6b2f37b481c9bb58
Raja Kolli <rajadilipkolli@gmail.com>
2025-02-15 17:39:43 +0530

    chore(deps): update mariadb docker tag to v11.7

--- jpa/boot-data-customsequence/src/test/java/com/example/custom/sequence/common/ContainersConfig.java
+++ jpa/boot-data-customsequence/src/test/java/com/example/custom/sequence/common/ContainersConfig.java
@@ -12,6 +12,6 @@ public class ContainersConfig {
     @Bean
     @ServiceConnection
     MariaDBContainer<?> mariaDbContainer() {
-        return new MariaDBContainer<>(DockerImageName.parse("mariadb").withTag("11.6"));
+        return new MariaDBContainer<>(DockerImageName.parse("mariadb").withTag("11.7"));
     }
 }
