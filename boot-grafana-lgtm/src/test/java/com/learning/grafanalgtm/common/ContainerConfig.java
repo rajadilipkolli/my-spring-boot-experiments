36461e5cf998a9c068bc279af418a4579609dfcc
Raja Kolli <rajadilipkolli@gmail.com>
2025-03-11 18:00:34 +0530

    upgrade version in testcontainers

--- boot-grafana-lgtm/src/test/java/com/learning/grafanalgtm/common/ContainerConfig.java
+++ boot-grafana-lgtm/src/test/java/com/learning/grafanalgtm/common/ContainerConfig.java
@@ -13,7 +13,7 @@ public class ContainerConfig {
     @Bean
     @ServiceConnection
     LgtmStackContainer lgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.8.6"))
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.9.1"))
                 .withStartupTimeout(Duration.ofMinutes(2));
     }
 }
