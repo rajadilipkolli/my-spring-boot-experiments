ccbc39a8c00a3b6a61d9a44be91e33ff637ca32a
Raja Kolli <rajadilipkolli@gmail.com>
2025-05-08 10:20:17 +0530

    Update version in  tests

--- jpa/boot-hibernate2ndlevelcache-sample/src/test/java/com/example/hibernatecache/common/ContainersConfig.java
+++ jpa/boot-hibernate2ndlevelcache-sample/src/test/java/com/example/hibernatecache/common/ContainersConfig.java
@@ -22,7 +22,7 @@ public class ContainersConfig {
     @Bean
     RedisContainer redisContainer() throws IOException {
         RedisContainer redisContainer =
-                new RedisContainer(DockerImageName.parse("redis").withTag("7.4.3-alpine"));
+                new RedisContainer(DockerImageName.parse("redis").withTag("8.0.0-alpine"));
         redisContainer.start();
         String ymlContent = """
                 singleServerConfig:
