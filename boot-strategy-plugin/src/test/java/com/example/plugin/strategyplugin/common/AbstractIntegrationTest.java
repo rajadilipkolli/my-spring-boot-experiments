929a81e069b8505e5d03533aa69a3530011ac384
Raja Kolli <rajadilipkolli@gmail.com>
2025-06-28 23:57:09 +0530

    feat : adds loki test and tc in dev mode (#1930)

---
+++ boot-strategy-plugin/src/test/java/com/example/plugin/strategyplugin/common/AbstractIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.example.plugin.strategyplugin.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ActiveProfiles({"test"})
+@SpringBootTest(
+        webEnvironment = RANDOM_PORT
+//        classes = {ContainerConfig.class}
+)
+@AutoConfigureMockMvc
+public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    protected MockMvcTester mockMvcTester;
+
+}
