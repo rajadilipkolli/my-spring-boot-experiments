46ca5a963d44efbc0b615d22ca77a6ddd37cf862
Raja Kolli <rajadilipkolli@gmail.com>
2025-07-07 18:51:32 +0000

    fix compilation issue

--- scheduler/boot-scheduler-quartz/src/main/java/com/scheduler/quartz/model/response/ScheduleJob.java
+++ scheduler/boot-scheduler-quartz/src/main/java/com/scheduler/quartz/model/response/ScheduleJob.java
@@ -3,8 +3,15 @@ package com.scheduler.quartz.model.response;
 import jakarta.validation.constraints.NotBlank;
 
 public record ScheduleJob(
+        String jobId,
         @NotBlank(message = "Job Name can't be blank") String jobName,
         String jobGroup,
         String jobStatus,
         String cronExpression,
-        String desc) {}
+        String desc) {
+    public ScheduleJob withJobId(final String jobId) {
+        return this.jobId == jobId
+                ? this
+                : new ScheduleJob(jobId, this.jobName, this.jobGroup, this.jobStatus, this.cronExpression, this.desc);
+    }
+}
