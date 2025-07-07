40a8559ae528dcbcf56d109e1569df8ecec27e0c
Raja Kolli <rajadilipkolli@gmail.com>
2025-07-07 19:28:22 +0000

    refactor: clean up annotations and imports in mapper and test classes

--- boot-api-archunit-sample/src/test/java/com/example/archunit/SchemaValidationTest.java
+++ boot-api-archunit-sample/src/test/java/com/example/archunit/SchemaValidationTest.java
@@ -10,8 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-@Import(ContainersConfig.class)
 @DataJpaTest(showSql = false, properties = "spring.jpa.hibernate.ddl-auto=validate")
+@Import(ContainersConfig.class)
 class SchemaValidationTest {
 
     @Autowired
--- graphql/boot-graphql-webmvc/src/main/java/com/example/graphql/mapper/PostCommentRequestToEntityMapper.java
+++ graphql/boot-graphql-webmvc/src/main/java/com/example/graphql/mapper/PostCommentRequestToEntityMapper.java
@@ -3,10 +3,14 @@ package com.example.graphql.mapper;
 import com.example.graphql.entities.PostCommentEntity;
 import com.example.graphql.model.request.PostCommentRequest;
 import com.example.graphql.repositories.PostRepository;
-import org.mapstruct.*;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 // After Mapping will not be set if we use builder pattern, hence disabled it
-@Mapper(builder = @Builder(disableBuilder = true))
+@Mapper
 public interface PostCommentRequestToEntityMapper {
 
     @Mapping(
