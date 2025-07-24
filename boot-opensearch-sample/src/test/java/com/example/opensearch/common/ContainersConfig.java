235ed2b8b5a5a308cb88c12b94c15cc3a46049c1
Raja Kolli <rajadilipkolli@gmail.com>
2025-07-24 23:09:43 +0530

    fix : typo

--- boot-opensearch-sample/src/test/java/com/example/opensearch/common/ContainersConfig.java
+++ boot-opensearch-sample/src/test/java/com/example/opensearch/common/ContainersConfig.java
@@ -1,7 +1,7 @@
 package com.example.opensearch.common;
 
 import java.time.Duration;
-import org.opensearch.testcontainers.OpensearchContainer;
+import org.opensearch.testcontainers.OpenSearchContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +11,8 @@ public class ContainersConfig {
 
     @Bean
     @ServiceConnection
-    OpensearchContainer<?> opensearchContainer() {
-        return new OpensearchContainer<>("opensearchproject/opensearch:3.1.0")
+    OpenSearchContainer<?> opensearchContainer() {
+        return new OpenSearchContainer<>("opensearchproject/opensearch:3.1.0")
                 .withStartupAttempts(5)
                 .withStartupTimeout(Duration.ofMinutes(10));
     }
