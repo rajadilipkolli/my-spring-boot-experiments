f2657ca9654281e1d74aafbffaf66f2302fb4d0f
Raja Kolli <rajadilipkolli@gmail.com>
2025-07-26 08:21:22 +0000

    fix : circleCI and upgrade to SB 4.x

--- graphql/boot-graphql-webflux/src/main/java/com/example/graphql/exception/GlobalErrorWebExceptionHandler.java
+++ graphql/boot-graphql-webflux/src/main/java/com/example/graphql/exception/GlobalErrorWebExceptionHandler.java
@@ -2,8 +2,8 @@ package com.example.graphql.exception;
 
 import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.WebProperties;
-import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
-import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.boot.webflux.autoconfigure.error.DefaultErrorWebExceptionHandler;
+import org.springframework.boot.webflux.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.codec.ServerCodecConfigurer;
