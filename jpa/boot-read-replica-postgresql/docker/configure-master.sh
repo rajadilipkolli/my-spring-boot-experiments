327b79c892751b558bce01d0a2a8a78b540306d0
Raja Kolli <rajadilipkolli@gmail.com>
2025-08-31 16:51:07 +0000

    solves bash

--- jpa/boot-read-replica-postgresql/docker/configure-master.sh
+++ jpa/boot-read-replica-postgresql/docker/configure-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
 echo "Configuring PostgreSQL master for replication..."
 
--- jpa/boot-read-replica-postgresql/docker/setup-slave.sh
+++ jpa/boot-read-replica-postgresql/docker/setup-slave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
 export PGDATA=/var/lib/postgresql/data
 
