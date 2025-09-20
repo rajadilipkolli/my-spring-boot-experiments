53a6286ff18c2672c1ebd483fd52ce67be3cafc5
Raja Kolli <rajadilipkolli@gmail.com>
2025-09-20 14:07:39 +0000

    chore(deps) : upgrade to spring boot 4.0.0-M3

--- boot-ultimate-redis/src/test/java/com/example/ultimateredis/common/AbstractIntegrationTest.java
+++ boot-ultimate-redis/src/test/java/com/example/ultimateredis/common/AbstractIntegrationTest.java
@@ -4,12 +4,12 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import com.example.ultimateredis.repository.ActorRepository;
 import com.example.ultimateredis.utils.AppConstants;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import tools.jackson.databind.ObjectMapper;
 
 @ActiveProfiles({AppConstants.PROFILE_STANDALONE})
 @SpringBootTest(
