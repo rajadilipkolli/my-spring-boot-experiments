d59d422d64614c46f2564c3dbefaf8fe4c0214e0
Raja Kolli <rajadilipkolli@gmail.com>
2025-10-01 15:51:02 +0000

    fix : issue with upgrade

--- batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/common/ContainersConfig.java
+++ batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/common/ContainersConfig.java
@@ -12,6 +12,6 @@ public class ContainersConfig {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("17.6-alpine"));
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("18.0-alpine"));
     }
 }
--- batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/web/controllers/CustomerControllerTest.java
+++ batch-boot-jpa-sample/src/test/java/com/example/bootbatchjpa/web/controllers/CustomerControllerTest.java
@@ -116,15 +116,15 @@ class CustomerControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isBadRequest())
-                .andExpect(header().string("Content-Type", is("application/problem+json")))
+                .andExpect(header().string("Content-Type", is(MediaType.APPLICATION_PROBLEM_JSON_VALUE)))
                 .andExpect(jsonPath("$.type", is("https://boot-jpa.com/errors/validation")))
                 .andExpect(jsonPath("$.title", is("Constraint Violation")))
                 .andExpect(jsonPath("$.status", is(400)))
                 .andExpect(jsonPath("$.detail", is("Invalid request content.")))
                 .andExpect(jsonPath("$.instance", is("/api/customers")))
-                .andExpect(jsonPath("$.violations", hasSize(1)))
-                .andExpect(jsonPath("$.violations[0].field", is("name")))
-                .andExpect(jsonPath("$.violations[0].message", is("Name cannot be empty")))
+                .andExpect(jsonPath("$.properties.violations", hasSize(1)))
+                .andExpect(jsonPath("$.properties.violations[0].field", is("name")))
+                .andExpect(jsonPath("$.properties.violations[0].message", is("Name cannot be empty")))
                 .andReturn();
     }
 
