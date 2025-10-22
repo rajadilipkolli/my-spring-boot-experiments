b602e3604ab86032f4708659165d3e5f1385a003
Raja Kolli <rajadilipkolli@gmail.com>
2025-10-22 18:18:47 +0000

    register ConversionServiceAdapter

--- graphql/boot-graphql-webmvc/src/main/java/com/example/graphql/mapper/MapperSpringConfig.java
+++ graphql/boot-graphql-webmvc/src/main/java/com/example/graphql/mapper/MapperSpringConfig.java
@@ -4,6 +4,6 @@ import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.extensions.spring.SpringMapperConfig;
 
-@MapperConfig(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@MapperConfig(uses = ConversionServiceAdapter.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 @SpringMapperConfig(conversionServiceBeanName = "appConversionService")
 public class MapperSpringConfig {}
