47e7322c859b6e37bdc94f6cfc1383bfde4788db
Raja Kolli <rajadilipkolli@gmail.com>
2025-10-24 09:51:19 +0000

    upgrade to RC1

--- boot-grafana-lgtm/src/test/java/com/learning/grafanalgtm/common/ContainerConfig.java
+++ boot-grafana-lgtm/src/test/java/com/learning/grafanalgtm/common/ContainerConfig.java
@@ -13,7 +13,7 @@ public class ContainerConfig {
     @Bean
     @ServiceConnection
     LgtmStackContainer lgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.11.12"))
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.11.13"))
                 .withStartupTimeout(Duration.ofMinutes(2));
     }
 }
