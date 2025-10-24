6b3a7cc6f37e60ab49cbe9010fabe11f29a0d996
Raja Kolli <rajadilipkolli@gmail.com>
2025-10-24 16:01:12 +0000

    chore(deps) : Upgrade to spring boot 4.0.0-RC1

--- jpa/boot-data-multipledatasources/src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
+++ jpa/boot-data-multipledatasources/src/test/java/com/example/multipledatasources/TestMultipleDataSourcesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 
 class TestMultipleDataSourcesApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.from(MultipleDataSourcesApplication::main)
                 .with(ContainersConfiguration.class)
                 .run(args);
