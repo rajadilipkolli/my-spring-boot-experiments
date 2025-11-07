259bd5ce59a510b89626b86de12fcac42404e8f0
Raja Kolli <rajadilipkolli@gmail.com>
2025-11-07 17:31:34 +0000

    fix build failure

--- r2dbc/boot-r2dbc-json-column/src/test/java/com/example/learning/common/AbstractIntegrationTest.java
+++ r2dbc/boot-r2dbc-json-column/src/test/java/com/example/learning/common/AbstractIntegrationTest.java
@@ -5,7 +5,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.micrometer.tracing.test.autoconfigure.AutoConfigureTracing;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.boot.webtestclient.AutoConfigureWebTestClient;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT, classes = ContainerConfig.class)
