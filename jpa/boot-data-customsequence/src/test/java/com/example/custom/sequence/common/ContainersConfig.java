caa306c0cf6322492a5505302e6d8dcde9c7ea4e
Raja Kolli <rajadilipkolli@gmail.com>
2025-11-21 05:20:49 +0000

    upgrade versions in test as well

--- jpa/boot-data-customsequence/src/test/java/com/example/custom/sequence/common/ContainersConfig.java
+++ jpa/boot-data-customsequence/src/test/java/com/example/custom/sequence/common/ContainersConfig.java
@@ -12,6 +12,6 @@ public class ContainersConfig {
     @Bean
     @ServiceConnection
     MariaDBContainer<?> mariaDbContainer() {
-        return new MariaDBContainer<>(DockerImageName.parse("mariadb").withTag("12.0"));
+        return new MariaDBContainer<>(DockerImageName.parse("mariadb").withTag("12.1"));
     }
 }
