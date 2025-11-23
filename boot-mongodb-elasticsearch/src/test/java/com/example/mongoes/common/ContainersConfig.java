2e92480f713f5bf5fe3427ff4863e994fc415fb7
Raja Kolli <rajadilipkolli@gmail.com>
2025-11-23 05:51:25 +0000

    attempt to upgrade

--- boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/common/ContainersConfig.java
+++ boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/common/ContainersConfig.java
@@ -5,8 +5,8 @@ import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.mongodb.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
