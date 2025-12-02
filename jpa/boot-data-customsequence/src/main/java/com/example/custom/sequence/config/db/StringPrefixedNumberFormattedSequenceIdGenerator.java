b74c635cd35c7845951a094d0a2651eb64441a72
Raja Kolli <rajadilipkolli@gmail.com>
2025-12-02 15:54:05 +0000

    Attempr to fix

--- jpa/boot-data-customsequence/src/main/java/com/example/custom/sequence/config/db/StringPrefixedNumberFormattedSequenceIdGenerator.java
+++ jpa/boot-data-customsequence/src/main/java/com/example/custom/sequence/config/db/StringPrefixedNumberFormattedSequenceIdGenerator.java
@@ -2,15 +2,10 @@ package com.example.custom.sequence.config.db;
 
 import java.io.Serializable;
 import java.lang.reflect.Member;
-import java.util.Properties;
 import org.hibernate.HibernateException;
-import org.hibernate.MappingException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.generator.GeneratorCreationContext;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.type.Type;
-import org.hibernate.type.spi.TypeConfiguration;
 
 public class StringPrefixedNumberFormattedSequenceIdGenerator extends SequenceStyleGenerator {
 
@@ -18,19 +13,13 @@ public class StringPrefixedNumberFormattedSequenceIdGenerator extends SequenceSt
     private final String numberFormat;
 
     public StringPrefixedNumberFormattedSequenceIdGenerator(
-            StringPrefixedSequence config, Member annotatedMember, GeneratorCreationContext context) {
-        valuePrefix = config.valuePrefix();
-        numberFormat = config.numberFormat();
+            StringPrefixedSequence annotation, Member annotatedMember, GeneratorCreationContext context) {
+        valuePrefix = annotation.valuePrefix();
+        numberFormat = annotation.numberFormat();
     }
 
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
         return valuePrefix + numberFormat.formatted(super.generate(session, object));
-    }
-
-    @Override
-    public void configure(Type type, Properties params, ServiceRegistry serviceRegistry) throws MappingException {
-        super.configure(
-                new TypeConfiguration().getBasicTypeRegistry().getRegisteredType(Long.class), params, serviceRegistry);
     }
 }
