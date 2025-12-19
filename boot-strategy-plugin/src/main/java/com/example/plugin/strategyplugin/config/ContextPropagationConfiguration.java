42a82fdd4c0f653404cb94e5c928f648dbe37e14
Raja Kolli <rajadilipkolli@gmail.com>
2025-12-19 12:40:40 +0000

    chore(deps) : upgrade spotless and add filters

---
+++ boot-strategy-plugin/src/main/java/com/example/plugin/strategyplugin/config/ContextPropagationConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.plugin.strategyplugin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.support.ContextPropagatingTaskDecorator;
+
+// When using @Async methods or AsyncTaskExecutor, context gets lost in new threads. Fix this by
+// configuring the
+// ContextPropagatingTaskDecorator
+@Configuration(proxyBeanMethods = false)
+public class ContextPropagationConfiguration {
+
+    @Bean
+    ContextPropagatingTaskDecorator contextPropagatingTaskDecorator() {
+        return new ContextPropagatingTaskDecorator();
+    }
+}
