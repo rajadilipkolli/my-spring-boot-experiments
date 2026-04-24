29d0977b60c7ec48b8f20d0745dab39e89201950
Raja Kolli <rajadilipkolli@gmail.com>
2026-04-24 15:26:32 +0000

    fix : issue with query counting

--- jpa/boot-data-customsequence/src/main/java/com/example/custom/sequence/config/db/JpaConfig.java
+++ jpa/boot-data-customsequence/src/main/java/com/example/custom/sequence/config/db/JpaConfig.java
@@ -1,6 +1,9 @@
 package com.example.custom.sequence.config.db;
 
 import io.hypersistence.utils.spring.repository.BaseJpaRepositoryImpl;
+import net.ttddyy.dsproxy.listener.DataSourceQueryCountListener;
+import net.ttddyy.observation.boot.autoconfigure.ProxyDataSourceBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -8,4 +11,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(
         repositoryBaseClass = BaseJpaRepositoryImpl.class,
         basePackages = "com.example.custom.sequence.repositories")
-public class JpaConfig {}
+public class JpaConfig {
+
+    @Bean
+    ProxyDataSourceBuilderCustomizer myCustomizer() {
+        return (builder, dataSource, beanName, dataSourceName) -> {
+            builder.listener(new DataSourceQueryCountListener());
+            builder.name("customSeqDsProxy");
+        };
+    }
+}
--- jpa/boot-data-customsequence/src/test/java/com/example/custom/sequence/ApplicationIntegrationTest.java
+++ jpa/boot-data-customsequence/src/test/java/com/example/custom/sequence/ApplicationIntegrationTest.java
@@ -3,13 +3,17 @@ package com.example.custom.sequence;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.custom.sequence.common.AbstractIntegrationTest;
+import javax.sql.DataSource;
 import net.ttddyy.dsproxy.support.ProxyDataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
         assertThat(dataSource).isInstanceOf(ProxyDataSource.class);
+        DataSource unwrapped = ((ProxyDataSource) dataSource).getDataSource();
+        assertThat(unwrapped).isInstanceOf(LazyConnectionDataSourceProxy.class);
     }
 }
