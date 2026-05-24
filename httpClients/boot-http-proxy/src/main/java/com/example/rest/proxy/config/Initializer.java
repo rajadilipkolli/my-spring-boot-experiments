d1226190a085e59cb80db4960c03cbe786d71159
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-24 19:51:08 +0530

    restore  logbook-spring-boot-starter functionality (#2484)
    * restore  logbook-spring-boot-starter functionality
    
    * Implement code review comment
    
    * implement code review comments

--- httpClients/boot-http-proxy/src/main/java/com/example/rest/proxy/config/Initializer.java
+++ httpClients/boot-http-proxy/src/main/java/com/example/rest/proxy/config/Initializer.java
@@ -3,16 +3,13 @@ package com.example.rest.proxy.config;
 import com.example.rest.proxy.client.JsonPlaceholderService;
 import com.example.rest.proxy.entities.Post;
 import com.example.rest.proxy.repositories.PostRepository;
-import com.example.rest.proxy.utils.AppConstants;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile(AppConstants.PROFILE_NOT_TEST)
 class Initializer implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(Initializer.class);
@@ -28,7 +25,15 @@ class Initializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         log.info("Running Initializer.....");
+        if (postRepository.count() > 0) {
+            log.info("Skipping initializer because posts already exist");
+            return;
+        }
+
         List<Post> postList = jsonPlaceholderService.loadAllPosts();
+        // Ensure ID is null to allow auto-generation
+        postList.forEach(post -> post.setId(null));
+
         this.postRepository.saveAll(postList);
         log.info("Completed Initializer.....");
     }
