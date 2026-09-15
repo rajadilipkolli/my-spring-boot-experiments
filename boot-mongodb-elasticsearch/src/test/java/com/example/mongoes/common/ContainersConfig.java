f2b32db1494bb114c8d95f3ab37288c7af4964cd
Raja Kolli <rajadilipkolli@gmail.com>
2026-09-15 08:35:42 +0530

    Update Elasticsearch and MongoDB container versions

--- boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/common/ContainersConfig.java
+++ boot-mongodb-elasticsearch/src/test/java/com/example/mongoes/common/ContainersConfig.java
@@ -16,7 +16,7 @@ public class ContainersConfig {
     @Bean
     @RestartScope
     ElasticsearchContainer elasticsearchContainer() {
-        return new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:9.5.2")
+        return new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:9.5.3")
                 .withEnv(Map.of("xpack.security.enabled", "false"));
     }
 
@@ -24,6 +24,6 @@ public class ContainersConfig {
     @Bean
     @RestartScope
     MongoDBContainer mongoDBContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.3.8")).withSharding();
+        return new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.3.11")).withSharding();
     }
 }
